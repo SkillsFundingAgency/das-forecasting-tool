73f6b33ebaf3cf425696976ec45f6b6978f0f82f
MarkFCain <mark.cain@alimar.co.uk>
2017-12-22 15:26:07 +0000

    CI-476 added data in at controller level using a model, and injecting into javascript chart

---
+++ src/SFA.DAS.Forecasting.Web/ViewModels/VisualisationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SFA.DAS.Forecasting.Web.ViewModels
+{
+    public class VisualisationViewModel
+    {
+        public string ChartTitle { get; set; }
+        public List<ChartItemViewModel> ChartItems { get; set; }
+
+    }
+}
