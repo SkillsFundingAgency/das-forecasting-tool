24132a0a1f9976a6c482d5bcacb458f7b9858964
josevi.agullo <josevi.agullo@gmail.com>
2018-01-23 15:09:44 +0000

    Adding new validators

---
+++ src/SFA.DAS.Forecasting.Payments.Application/Validation/CollectionPeriodSuperficialValidator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.Forecasting.Core.Validation;
+using SFA.DAS.Forecasting.Payments.Application.Messages;
+
+namespace SFA.DAS.Forecasting.Payments.Application.Validation
+{
+    public class CollectionPeriodSuperficialValidator : ISuperficialValidation<CollectionPeriod>
+    {
+        public List<ValidationFailure> Validate(CollectionPeriod itemToValidate)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/SFA.DAS.Forecasting.Payments.Application/Validation/EarningDetailsSuperficialValidator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.Forecasting.Core.Validation;
+using SFA.DAS.Forecasting.Payments.Application.Messages;
+
+namespace SFA.DAS.Forecasting.Payments.Application.Validation
+{
+    public class EarningDetailsSuperficialValidator : ISuperficialValidation<EarningDetails>
+    {
+        public List<ValidationFailure> Validate(EarningDetails itemToValidate)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
