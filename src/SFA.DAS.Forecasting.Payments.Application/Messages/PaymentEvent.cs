86cd3dc00fe560e12b750403c89b0da082b7029e
josevi.agullo <josevi.agullo@gmail.com>
2018-01-23 15:09:02 +0000

    Adding note into payment message class

--- src/SFA.DAS.Forecasting.Payments.Application/Messages/PaymentEvent.cs
+++ src/SFA.DAS.Forecasting.Payments.Application/Messages/PaymentEvent.cs
@@ -1,21 +1,26 @@
 ﻿namespace SFA.DAS.Forecasting.Payments.Application.Messages
 {
-    public class PaymentEvent
+	/// <summary>
+	/// TODO: Temp event definition. this will be replaced by the actual Payment event published by the employer services.
+	/// </summary>
+	public class PaymentEvent
     {
         public string Id { get; set; }
-        public string EmployerAccountId { get; set; }
-        public long Ukprn { get; set; }
+
+		public string EmployerAccountId { get; set; }
+
+		public long Ukprn { get; set; }
 
         public long? ApprenticeshipId { get; set; }
-        public long Uln { get; set; }
 
-        public int? ProgrammeType { get; set; }
-        public int? FrameworkCode { get; set; }
+	    public decimal Amount { get; set; }
 
+	    public EarningDetails EarningDetails { get; set; }
+	    public CollectionPeriod CollectionPeriod { get; set; }
+
+		public int? FrameworkCode { get; set; }
+	    public int? PathwayCode { get; set; }
+		public int? ProgrammeType { get; set; }
         public long? StandardCode { get; set; }
-        public decimal Amount { get; set; }
-        public int? PathwayCode { get; set; }
-        public EarningDetails EarningDetails { get; set; }
-        public CollectionPeriod CollectionPeriod { get; set; }
     }
 }
