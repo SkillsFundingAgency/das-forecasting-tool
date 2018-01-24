222b947b7500192638705a4ae07775c7a924f2ce
josevi.agullo <josevi.agullo@gmail.com>
2018-01-24 15:33:39 +0000

    Adding functions to read the payment events

---
+++ src/SFA.DAS.Forecasting.Payments.Functions/PaymentEventHttpFunction.cs
@@ -0,0 +1,31 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using SFA.DAS.Forecasting.Functions.Framework;
+using SFA.DAS.Forecasting.Payments.Application.Messages;
+
+namespace SFA.DAS.Forecasting.Payments.Functions
+{
+    public class PaymentEventHttpFunction : IFunction
+    {
+        [FunctionName("LevyDeclarationEventHttpFunction")]
+        [return: Queue(QueueNames.PaymentProcessor)]
+        public static async Task<PaymentEvent> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "LevyDeclarationEventHttpFunction")]HttpRequestMessage req, 
+            TraceWriter writer)
+        {
+            return await FunctionRunner.Run<PaymentEventHttpFunction, PaymentEvent>(writer,
+                async (container, logger) =>
+                {
+                    var body = await req.Content.ReadAsStringAsync();
+                    var paymentEvent = JsonConvert.DeserializeObject<PaymentEvent>(body);
+
+                    logger.Info($"Added one payment to {QueueNames.PaymentProcessor} queue.");
+                    return await Task.FromResult(paymentEvent);
+                });
+        }
+    }
+}
--- src/SFA.DAS.Forecasting.Payments.Functions/QueueNames.cs
+++ src/SFA.DAS.Forecasting.Payments.Functions/QueueNames.cs
@@ -1,6 +1,6 @@
 ﻿namespace SFA.DAS.Forecasting.Payments.Functions
 {
-    internal class QueueName
+    internal class QueueNames
     {
 	    public const string PaymentValidator = "forecasting-payment-validate-payment";
 	    public const string PaymentProcessor = "forecasting-payment-process-payment";
