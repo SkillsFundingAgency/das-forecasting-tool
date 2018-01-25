31be580d59de00035d3d4e280ccae3f1a78579e1
froberg <christofer.froberg@gmail.com>
2018-01-25 12:00:39 +0000

    Added Levy AC tests as specflow tests

---
+++ src/SFA.DAS.Forecasting.AcceptanceTests/Levy/Feature/ProcessLevyEvent.feature
@@ -0,0 +1,11 @@
+﻿Feature: ProcessLevyEvent [CI-528]
+	As an employer
+	I want my levy credit to be forecast for the next 4 years
+	So that I can effectively forecast my account balance
+
+Scenario: AC1 Store levy credit event data
+Given that I'm the ESFA
+And I have credited levy to employer accouns
+And levy credit events have been created
+Then the data for each levy credit event is stored
+And all of the data stored is correct
--- src/SFA.DAS.Forecasting.AcceptanceTests/Levy/Steps/ProcessLevyEventCI_528Steps.cs
+++ src/SFA.DAS.Forecasting.AcceptanceTests/Levy/Steps/ProcessLevyEventCI_528Steps.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using NUnit.Framework;
+using SFA.DAS.Forecasting.AcceptanceTests.Services;
 using SFA.DAS.Forecasting.Levy.Application.Messages;
 using System;
 using System.Collections.Generic;
@@ -7,60 +8,82 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using TechTalk.SpecFlow;
 
-namespace SFA.DAS.Forecasting.AcceptanceTests.Levy
+namespace SFA.DAS.Forecasting.AcceptanceTests.Levy.Steps
 {
-    [TestFixture]
-    public class AddingLevyDeclarations
+    [Binding]
+    public class ProcessLevyEventCI_528Steps
     {
         // ToDo: Move to config...
         private const string TableName = "LevyDeclarations";
         private const string ConnectionString = "UseDevelopmentStorage=true";
         private const long EmployerAccountId = 1111;
+        private string _baseUrl = "http://localhost:7071/api";
 
         private AzureTableService _azureTableService;
+        private List<LevyDeclarationEvent> _records;
 
-        [OneTimeSetUp]
-        public void SetUp()
+
+        [BeforeScenario]
+        public void BeforeScenario()
         {
             _azureTableService = new AzureTableService(ConnectionString, TableName);
             _azureTableService.EnsureExcists();
-
-            // Start Functions
         }
 
-        [OneTimeTearDown]
-        public void TearDown()
+        [AfterScenario]
+        public void AfterSecnario()
         {
             _azureTableService.DeleteEntities(EmployerAccountId.ToString());
         }
 
-        [Test]
-        public async Task Run()
+        [Given(@"that I'm the ESFA")]
+        public void GivenThatIMTheESFA()
         {
+            //ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have credited levy to employer accouns")]
+        public void GivenIHaveCreditedLevyToEmployerAccouns()
+        {
+            // ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"levy credit events have been created")]
+        public async Task GivenLevyCreditEventsHaveBeenCreated()
+        {
+
             var client = new HttpClient();
 
-            foreach(var item in Data())
+            foreach (var item in Data())
             {
-                await client.PostAsync("http://localhost:7071/api/LevyDeclarationEventHttpFunction", new StringContent(item));
+                await client.PostAsync($"{_baseUrl}/LevyDeclarationEventHttpFunction", new StringContent(item));
                 Thread.Sleep(100);
             }
+        }
+        
+        [Then(@"the data for each levy credit event is stored")]
+        public void ThenTheDataForEachLevyCreditEventIsStored()
+        {
 
-            Thread.Sleep(1000); // retry..
-
-            var records = _azureTableService.GetRecords(EmployerAccountId.ToString())
+            _records = _azureTableService.GetRecords(EmployerAccountId.ToString())
                 .ToList();
 
             double expectedRecordsoBeSaved = 2;
-            Assert.AreEqual(expectedRecordsoBeSaved, records.Count(), message: $"Only {expectedRecordsoBeSaved} record should validate and be saved to the database");
-
-            var second = records[1];
+            Assert.AreEqual(expectedRecordsoBeSaved, _records.Count(), message: $"Only {expectedRecordsoBeSaved} record should validate and be saved to the database");
+        }
+        
+        [Then(@"all of the data stored is correct")]
+        public void ThenAllOfTheDataStoredIsCorrect()
+        {
+            var second = _records[1];
             Assert.AreEqual(201, second.Amount);
         }
 
         private IEnumerable<string> Data()
         {
-            return 
+            return
                 new List<LevyDeclarationEvent> {
                     new LevyDeclarationEvent {
                         EmployerAccountId = EmployerAccountId,
