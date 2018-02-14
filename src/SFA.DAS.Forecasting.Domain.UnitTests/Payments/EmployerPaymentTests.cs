6df905dadb053dfe902e786c23e7f350774ea424
josevi.agullo <josevi.agullo@gmail.com>
2018-02-14 15:05:00 +0000

    Added AllowProjection function for Payments

--- src/SFA.DAS.Forecasting.Domain.UnitTests/Payments/EmployerPaymentTests.cs
+++ src/SFA.DAS.Forecasting.Domain.UnitTests/Payments/EmployerPaymentTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.Forecasting.Domain.Payments;
@@ -20,38 +23,72 @@ namespace SFA.DAS.Forecasting.Domain.UnitTests.Payments
             _service = new EmployerPayment(_levyRepository.Object);
         }
 
-        [Test]
-        public async Task Stores_Valid_Payment()
-        {
-	        var id = "testId";
-	        var employerAccountId = 123456;
+		[Test]
+		public async Task Stores_Valid_Payment()
+		{
+			var id = "testId";
+			var employerAccountId = 123456;
 			var ukprn = 123456;
-	        var apprenticeshipId = 654321;
-	        var amount = 123m;
+			var apprenticeshipId = 654321;
+			var amount = 123m;
 
-            Payment payment = null;
-            _levyRepository
+			Payment payment = null;
+			_levyRepository
 				.Setup(m => m.StorePayment(It.IsAny<Payment>()))
-                .Callback<Payment>(p => payment = p)
-                .Returns(Task.Run(() => 1));
+				.Callback<Payment>(p => payment = p)
+				.Returns(Task.Run(() => 1));
 
-            await _service.AddPayment(new Payment
-            {
-	            ExternalPaymentId = id,
+			await _service.AddPayment(new Payment
+			{
+				ExternalPaymentId = id,
 				EmployerAccountId = employerAccountId,
 				ProviderId = ukprn,
 				ApprenticeshipId = apprenticeshipId,
 				Amount = amount
-            });
+			});
 
-            Assert.AreEqual(id, payment.ExternalPaymentId);
-            Assert.AreEqual(employerAccountId, payment.EmployerAccountId);
-            Assert.AreEqual(ukprn, payment.ProviderId);
-            Assert.AreEqual(apprenticeshipId, payment.ApprenticeshipId);
-            Assert.AreEqual(amount, payment.Amount);
-			
-            _levyRepository.Verify(repo => repo.StorePayment(
-                    It.IsAny<Payment>()));
-        }
-    }
+			Assert.AreEqual(id, payment.ExternalPaymentId);
+			Assert.AreEqual(employerAccountId, payment.EmployerAccountId);
+			Assert.AreEqual(ukprn, payment.ProviderId);
+			Assert.AreEqual(apprenticeshipId, payment.ApprenticeshipId);
+			Assert.AreEqual(amount, payment.Amount);
+
+			_levyRepository.Verify(repo => repo.StorePayment(
+				It.IsAny<Payment>()));
+		}
+
+	    [Test]
+	    public void ReturnsCorrectLastTimeReceivedPayment()
+	    {
+		    var payments = new List<Payment>
+		    {
+				new Payment
+			    {
+					EmployerAccountId = 1,
+				    ReceivedTime = DateTime.Now.AddMinutes(-2)
+			    },
+			    new Payment
+			    {
+					EmployerAccountId = 2,
+				    ReceivedTime = DateTime.Now.AddMinutes(-1)
+			    },
+			    new Payment
+			    {
+					EmployerAccountId = 3,
+				    ReceivedTime = DateTime.Now.AddMinutes(-3)
+			    },
+			    new Payment
+			    {
+					EmployerAccountId = 4,
+				    ReceivedTime = DateTime.Now.AddMinutes(-5)
+			    },
+			};
+
+		    var lastReceivedTime = _service.GetLastTimeReceivedPayment(payments);
+
+			Assert.IsNotNull(lastReceivedTime);
+
+		    Assert.AreEqual(lastReceivedTime, payments.FirstOrDefault(x => x.EmployerAccountId == 2).ReceivedTime);
+	    }
+	}
 }
--- src/SFA.DAS.Forecasting.Payments.Functions/PaymentEventStoreFunction.cs
+++ src/SFA.DAS.Forecasting.Payments.Functions/PaymentEventStoreFunction.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using SFA.DAS.Forecasting.Application.Infrastructure.Configuration;
 using SFA.DAS.Forecasting.Application.Payments.Handlers;
-using SFA.DAS.Forecasting.Application.Payments.Mapping;
 using SFA.DAS.Forecasting.Application.Payments.Messages;
+using SFA.DAS.Forecasting.Application.Shared.Queues;
 using SFA.DAS.Forecasting.Functions.Framework;
 
 namespace SFA.DAS.Forecasting.Payments.Functions
@@ -22,6 +24,11 @@ namespace SFA.DAS.Forecasting.Payments.Functions
 	                var handler = container.GetInstance<ProcessEmployerPaymentHandler>();
 
 					await handler.Handle(paymentCreatedMessage);
+
+	                var queueService = container.GetInstance<QueueService>();
+	                var config = container.GetInstance<IApplicationConfiguration>();
+
+					queueService.SendMessageWithVisibilityDelay(paymentCreatedMessage, QueueNames.AllowProjection, TimeSpan.FromSeconds(config.SecondsToWaitToAllowProjections));
 
                     return await Task.FromResult(paymentCreatedMessage);
                 });
