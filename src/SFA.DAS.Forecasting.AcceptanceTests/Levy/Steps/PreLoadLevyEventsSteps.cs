65fcacd3c7a89f893eb9e4ef7c43f610e8463ff8
abroodie <abroodie@gmail.com>
2018-02-20 15:11:29 +0000

    Moved payments api config into seperate file

--- src/SFA.DAS.Forecasting.AcceptanceTests/Levy/Steps/PreLoadLevyEventsSteps.cs
+++ src/SFA.DAS.Forecasting.AcceptanceTests/Levy/Steps/PreLoadLevyEventsSteps.cs
@@ -1,4 +1,5 @@
-﻿using Dapper;
+﻿using System;
+using Dapper;
 using SFA.DAS.Forecasting.AcceptanceTests.EmployerApiStub;
 using System.Collections.Generic;
 using System.Data;
@@ -47,7 +48,7 @@ namespace SFA.DAS.Forecasting.AcceptanceTests.Levy.Steps
         public async Task ITriggerFunction()
         {
             var item = "{\"EmployerAccountIds\":[\"MJK9XV\",\"MGXLRV\",\"MPN4YM\"],\"PeriodYear\":\"18-19\",\"PeriodMonth\":1}";
-
+            Console.WriteLine($"Triggering Levy preload. Uri: {Config.LevyPreLoadFunctionUrl}, payload: {item}");
             var client = new HttpClient();
             await client.PostAsync(Config.LevyPreLoadFunctionUrl, new StringContent(item));
         }
---
+++ src/SFA.DAS.Forecasting.Application/Infrastructure/Configuration/PaymentsEventsApiConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using SFA.DAS.Provider.Events.Api.Client;
+
+namespace SFA.DAS.Forecasting.Application.Infrastructure.Configuration
+{
+    public class PaymentsEventsApiConfiguration : IPaymentsEventsApiConfiguration
+    {
+        public string ClientToken { get; set; }
+        public string ApiBaseUrl { get; set; }
+    }
+}
