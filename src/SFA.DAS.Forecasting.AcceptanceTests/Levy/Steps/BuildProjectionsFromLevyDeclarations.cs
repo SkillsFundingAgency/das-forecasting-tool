fb2e67c6f72310eee853be768d8c3cdeb26b6be6
abroodie <abroodie@gmail.com>
2018-02-20 23:16:50 +0000

    added acceptance test to cover levy declarations triggering projection generation

---
+++ src/SFA.DAS.Forecasting.AcceptanceTests/Levy/Steps/BuildProjectionsFromLevyDeclarations.cs
@@ -0,0 +1,22 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Forecasting.AcceptanceTests.Levy.Steps
+{
+    [Binding]
+    public class BuildProjectionsFromLevyDeclarations : StepsBase
+    {
+        [Scope(Feature = "Build Projections From Levy Declarations")]
+        [BeforeFeature(Order = 1)]
+        public static void StartLevyFunction()
+        {
+            StartFunction("SFA.DAS.Forecasting.Levy.Functions");
+            StartFunction("SFA.DAS.Forecasting.Projections.Functions");
+        }
+        
+        [Given(@"no account projections have been generated")]
+        public void GivenNoAccountProjectionsHaveBeenGenerated()
+        {
+            DeleteAccountProjections();
+        }
+    }
+}
