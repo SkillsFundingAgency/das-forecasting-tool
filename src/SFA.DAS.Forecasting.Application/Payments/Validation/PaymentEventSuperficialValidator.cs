d251cdb083077eabafd4b05e1fc13c2dce6f5282
josevi.agullo <josevi.agullo@gmail.com>
2018-02-22 15:10:48 +0000

    Fixed Payment validation

--- src/SFA.DAS.Forecasting.Application/Payments/Validation/PaymentEventSuperficialValidator.cs
+++ src/SFA.DAS.Forecasting.Application/Payments/Validation/PaymentEventSuperficialValidator.cs
@@ -11,19 +11,22 @@ namespace SFA.DAS.Forecasting.Application.Payments.Validation
             RuleFor(m => m.EmployerAccountId)
                 .NotNull().NotEmpty()
                 .GreaterThan(0);
+	        RuleFor(m => m.Ukprn).GreaterThan(0);
+	        RuleFor(m => m.ProviderName).NotNull().NotEmpty();
+	        RuleFor(m => m.ApprenticeshipId).GreaterThan(0);
+	        RuleFor(m => m.ApprenticeName).NotNull().NotEmpty();
+			RuleFor(m => m.Amount).GreaterThan(0);
 
-            RuleFor(m => m.Amount).GreaterThan(0);
-            RuleFor(m => m.ApprenticeshipId).GreaterThan(0);
-            RuleFor(m => m.Ukprn).GreaterThan(0);
-            RuleFor(m => m.FundingSource).Must(v => v.HasFlag(FundingSource.Levy) || v.HasFlag(FundingSource.Transfer));
-
-            RuleFor(m => m.EarningDetails)
+	        RuleFor(m => m.CourseName).NotNull().NotEmpty();
+			
+			RuleFor(m => m.EarningDetails)
                 .NotNull()
                 .SetValidator(new EarningDetailsSuperficialValidator());
 
             RuleFor(m => m.CollectionPeriod)
                 .NotNull()
                 .SetValidator(new CollectionPeriodSuperficialValidator());
-        }
-    }
+	        RuleFor(m => m.FundingSource).Must(v => v.HasFlag(FundingSource.Levy) || v.HasFlag(FundingSource.Transfer));
+		}
+	}
 }
