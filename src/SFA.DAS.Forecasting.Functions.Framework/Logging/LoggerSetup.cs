653c91702fbef0bc685eec7fd8f11241c0352e0f
froberg <christofer.froberg@gmail.com>
2018-03-16 10:13:07 +0000

    Moved nlog config to code (and settings.json)
    * removed nlog.config

--- src/SFA.DAS.Forecasting.Functions.Framework/Logging/LoggerSetup.cs
+++ src/SFA.DAS.Forecasting.Functions.Framework/Logging/LoggerSetup.cs
@@ -1,18 +1,69 @@
 ﻿using Microsoft.Azure.WebJobs.Host;
 using NLog;
+using NLog.Common;
 using NLog.Config;
+using NLog.Targets;
 using SFA.DAS.NLog.Logger;
 using System;
 using System.Configuration;
-using System.IO;
-using System.Reflection;
-using System.Threading;
 
 namespace SFA.DAS.Forecasting.Functions.Framework.Logging
 {
     public class LoggerSetup
-    {
-        private static void HookNLogToAzureLog(TraceWriter writer)
+    { 
+        internal static NLogLogger Create(string functionPath, TraceWriter writer, Type type)
+        {
+            var appName = GetSetting("AppName");
+            var localLogPath = GetSetting("NLog-Localdir");
+
+            LogManager.Configuration = new LoggingConfiguration();
+            LogManager.ThrowConfigExceptions = true;
+
+            if (IsDevEnvironment)
+                AddLocalTarget(localLogPath, appName);
+            else
+                AddRedisTarget(appName);
+
+            //AddAzureTarget(writer);
+            return new NLogLogger(type);
+        }
+
+        private static void AddRedisTarget(string appName)
+        {
+            var target = new RedisTarget
+            {
+                Name = "RedisLog",
+                AppName = appName,
+                EnvironmentKey = "EnvironmentName",
+                ConnectionStringKey = "LoggingRedisConnectionString",
+                IncludeAllProperties = true,
+                KeySettingsKey = "LoggingRedisKey",
+                Layout = "${message}"
+
+            };
+
+            LogManager.Configuration.AddTarget(target);
+            LogManager.Configuration.AddRule(LogLevel.Info, LogLevel.Fatal, "RedisLog");
+        }
+
+        private static void AddLocalTarget(string localLogPath, string appName)
+        {
+            var logFilePath = GetSetting("NLog-InternalLogFile");
+            var layout = GetSetting("NLog-SimpleLayout");
+
+            InternalLogger.LogFile = logFilePath.Replace("{APPNAME}", appName);
+
+            var fileTarget = new FileTarget("Disk")
+            {
+                FileName = localLogPath + "/logs/${shortdate}/" + appName + ".${shortdate}.log",
+                Layout = layout
+            };
+
+            LogManager.Configuration.AddTarget(fileTarget);
+            LogManager.Configuration.AddRule(LogLevel.Trace, LogLevel.Fatal, "Disk");
+        }
+
+        private static void AddAzureTarget(TraceWriter writer)
         {
             var config = LogManager.Configuration ?? new LoggingConfiguration();
 
@@ -27,22 +78,12 @@ namespace SFA.DAS.Forecasting.Functions.Framework.Logging
             LogManager.Configuration = config;
         }
 
-        internal static NLogLogger Create(string functionPath, TraceWriter writer, Type type)
+        private static string GetSetting(string key)
         {
-            LogManager.ThrowConfigExceptions = true;
-            LogManager.Configuration = new XmlLoggingConfiguration(Path.Combine(functionPath, "NLog.config"));
-
-            if (IsDevEnvironment)
-            {
-                LogManager.Configuration.AddRule(LogLevel.Info, LogLevel.Fatal, "Disk");
-            }
-
-            HookNLogToAzureLog(writer);
-            var logger = new NLogLogger(type);
-            return logger;
+            return ConfigurationManager.AppSettings[key];
         }
 
-        public static bool IsDevEnvironment =>
+        private static bool IsDevEnvironment =>
             (ConfigurationManager.AppSettings["EnvironmentName"]?.Equals("DEV") ?? false) ||
             (ConfigurationManager.AppSettings["EnvironmentName"]?.Equals("DEVELOPMENT") ?? false) ||
             (ConfigurationManager.AppSettings["EnvironmentName"]?.Equals("LOCAL") ?? false);
