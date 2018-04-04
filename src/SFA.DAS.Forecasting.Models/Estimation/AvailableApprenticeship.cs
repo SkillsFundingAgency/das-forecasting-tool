f4a5a672b13101b41a87e6e90921fc5b6f39c5b4
markcain <mark.cain@digital.education.gov.uk>
2018-04-04 16:21:24 +0100

    CIA-604 added first pass of controller, viewmodel and page for add apprenticeship

---
+++ src/SFA.DAS.Forecasting.Models/Estimation/AvailableApprenticeship.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.DAS.Forecasting.Models.Estimation
+{
+    public class AvailableApprenticeship
+    {
+
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public float Cap { get; set; }
+
+        public int Duration { get; set; }
+    }
+}
