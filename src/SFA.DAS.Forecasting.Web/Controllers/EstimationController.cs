8f624b7c6e4d9c7ae285a45eaa87ac1f381f6fd0
kola-samuel <samuel.dev.dba@gmail.com>
2018-04-10 12:58:34 +0100

    Start Estimation Page Code changes

--- src/SFA.DAS.Forecasting.Web/Controllers/EstimationController.cs
+++ src/SFA.DAS.Forecasting.Web/Controllers/EstimationController.cs
@@ -21,12 +21,35 @@ namespace SFA.DAS.Forecasting.Web.Controllers
         }
 
         [HttpGet]
+        [Route("estimations/start-transfer", Name = "EstimationStart")]
+        public async Task<ActionResult> StartEstimation(string hashedAccountId, string estimateName)
+        {
+            var viewModel = await _orchestrator.CostEstimation(hashedAccountId, estimateName);
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        [Route("estimations/start-redirect", Name = "EstimationStartRedirect")]
+        public ActionResult RedirectEstimationStart(string hashedAccountId)
+        {
+            var foundEstimation = true;
+
+            if (foundEstimation)
+            {
+                return RedirectToAction("CostEstimation", new { hashedaccountId = hashedAccountId, estimateName = "default" });
+            }
+
+            return RedirectToAction("", "", null);
+        }
+
+        [HttpGet]
         [Route("estimations/{estimateName}", Name = "EstimatedCost")]
         public async Task<ActionResult> CostEstimation(string hashedAccountId, string estimateName)
         {
             var viewModel = await _orchestrator.CostEstimation(hashedAccountId, estimateName);
             return View(viewModel);
         }
+
 
     }
 
