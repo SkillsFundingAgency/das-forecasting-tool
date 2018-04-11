d6c91c1fa79085fd526e083bbd95956eb229d593
kola-samuel <samuel.dev.dba@gmail.com>
2018-04-11 15:11:28 +0100

    Start Estimation Code changes

--- src/SFA.DAS.Forecasting.Web/Controllers/EstimationController.cs
+++ src/SFA.DAS.Forecasting.Web/Controllers/EstimationController.cs
@@ -8,7 +8,7 @@ namespace SFA.DAS.Forecasting.Web.Controllers
 {
     [ValidateMembership]
     [AuthorizeForecasting]
-    [RoutePrefixAttribute("accounts/{hashedaccountId}/forecasting")]
+    [RoutePrefix("accounts/{hashedaccountId}/forecasting")]
     public class EstimationController : Controller
     {
         private readonly IEstimationOrchestrator _orchestrator;
@@ -30,13 +30,13 @@ namespace SFA.DAS.Forecasting.Web.Controllers
 
         [HttpGet]
         [Route("estimations/start-redirect", Name = "EstimationStartRedirect")]
-        public ActionResult RedirectEstimationStart(string hashedAccountId)
+        public async Task<ActionResult> RedirectEstimationStart(string hashedAccountId)
         {
-            var accountEstimation = _orchestrator.GetEstimation(hashedAccountId);
+            var accountEstimation = await _orchestrator.GetEstimation(hashedAccountId);
 
-            if (accountEstimation != null)
+            if (accountEstimation != null && accountEstimation.HasValidApprenticeships)
             {
-                return RedirectToAction(nameof(CostEstimation), new { hashedaccountId = hashedAccountId, estimateName = "default" });
+                return RedirectToAction(nameof(CostEstimation), new { hashedaccountId = hashedAccountId, estimateName = accountEstimation.Name });
             }
 
             return RedirectToRoute("AddApprenticeships",new { hashedAccountId });
