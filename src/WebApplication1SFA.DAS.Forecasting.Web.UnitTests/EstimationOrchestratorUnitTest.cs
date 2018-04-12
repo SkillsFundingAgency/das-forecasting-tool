8532a701c113f9971a21728127cdfc73301909de
markcain <mark.cain@digital.education.gov.uk>
2018-04-12 09:31:35 +0100

    CI-701 minor renaming

--- src/WebApplication1SFA.DAS.Forecasting.Web.UnitTests/EstimationOrchestratorUnitTest.cs
+++ src/WebApplication1SFA.DAS.Forecasting.Web.UnitTests/EstimationOrchestratorUnitTest.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
-using SFA.DAS.Forecasting.Application.Estimations.Service;
 using Moq;
 using AutoMoq;
 using SFA.DAS.Forecasting.Models.Estimation;
