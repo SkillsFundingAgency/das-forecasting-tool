b93fd552f12233840ef2b393e6b3edad72a6951a
RAJESHVELUC560B\rajesh-grace <rajesh@digi2al.co.uk>
2018-04-12 08:58:19 +0100

    Select2 javascript solution implemented for apprentice search.  HTML markup added.

--- src/SFA.DAS.Forecasting.Web/App_Start/BundleConfig.cs
+++ src/SFA.DAS.Forecasting.Web/App_Start/BundleConfig.cs
@@ -10,14 +10,16 @@ namespace SFA.DAS.Forecasting.Web
             bundles.Add(new StyleBundle("~/css/bundles/screenie6").Include("~/Content/dist/css/screen-ie6.css"));
             bundles.Add(new StyleBundle("~/css/bundles/screenie7").Include("~/Content/dist/css/screen-ie7.css"));
             bundles.Add(new StyleBundle("~/css/bundles/screenie8").Include("~/Content/dist/css/screen-ie8.css"));
-            bundles.Add(new StyleBundle("~/css/bundles/screen").Include("~/Content/dist/css/screen.css"));
-
             bundles.Add(new StyleBundle("~/css/bundles/c3").Include("~/Content/dist/c3/c3.min.css"));
+            bundles.Add(new StyleBundle("~/css/bundles/select2").Include("~/Content/dist/css/select2.min.css"));
+            bundles.Add(new StyleBundle("~/css/bundles/screen").Include("~/Content/dist/css/screen.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/libs")
                 .Include("~/Content/dist/js/jquery-1.10.2.min.js")
                 .Include("~/Content/dist/js/stacker.js")
-                .Include("~/Content/dist/js/showhide-content.js"));
+                .Include("~/Content/dist/js/showhide-content.js")
+                 .Include("~/Content/dist/js/select2.min.js")
+                .Include("~/Content/dist/js/custom.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/c3")
                 .Include("~/Content/dist/c3/d3.min.js")
---
+++ src/SFA.DAS.Forecasting.Web/Content/dist/js/custom.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    var init = function () {
+        if ($("#chosse-apprenticeship")) {
+            $("#chosse-apprenticeship").select2();
+        }
+    };
+    init(); 
+
+    // open dropdownon on focus
+    $(document).on('focus', '.select2', function () {
+        $(this).siblings('select').select2('open');
+    });
+
+    // retain tabbed order after selection
+    $('#chosse-apprenticeship').on('select2:select', function () {
+        $("#no-of-app").focus();
+    });
+
+    // retain tabbed order on close without selection
+    $('#chosse-apprenticeship').on('select2:close', function () {
+        $("#no-of-app").focus();
+    });
+
+}());
