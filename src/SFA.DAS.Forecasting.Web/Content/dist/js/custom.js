276d44d6bfca51cebe662e90ba0db735245b27ff
markcain <mark.cain@digital.education.gov.uk>
2018-05-09 15:24:55 +0100

    CI-801 updated names in controls and class names to suit purpose

--- src/SFA.DAS.Forecasting.Web/Content/dist/js/custom.js
+++ src/SFA.DAS.Forecasting.Web/Content/dist/js/custom.js
@@ -1,7 +1,7 @@
 ﻿(function () {
     var init = function () {
-        if ($("#chosse-apprenticeship")) {
-            $("#chosse-apprenticeship").select2();
+        if ($("#choose-apprenticeship")) {
+            $("#choose-apprenticeship").select2();
         }
     };
     init();
@@ -12,12 +12,12 @@
     });
 
     // retain tabbed order after selection
-    $('#chosse-apprenticeship').on('select2:select', function () {
+    $('#choose-apprenticeship').on('select2:select', function () {
         $("#no-of-app").focus();
     });
 
     // retain tabbed order on close without selection
-    $('#chosse-apprenticeship').on('select2:close', function () {
+    $('#choose-apprenticeship').on('select2:close', function () {
         $("#no-of-app").focus();
     });
 
