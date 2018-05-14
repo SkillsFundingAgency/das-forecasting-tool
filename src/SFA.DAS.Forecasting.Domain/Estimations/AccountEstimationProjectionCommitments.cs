ec5eef1a2da095c212556b1f17aa9b7b85196d76
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-05-14 08:42:38 +0100

    Add AccountEstimationProjectionCommitments class
    Added new AccountEstimationProjectionCommitments class as a container
    for the VirtualEmployerCommitments and the TransferEmployerCommitments.

---
+++ src/SFA.DAS.Forecasting.Domain/Estimations/AccountEstimationProjectionCommitments.cs
@@ -0,0 +1,16 @@
+﻿using SFA.DAS.Forecasting.Domain.Commitments;
+
+namespace SFA.DAS.Forecasting.Domain.Estimations
+{
+    public class AccountEstimationProjectionCommitments
+    {
+        public EmployerCommitments VirtualEmployerCommitments { get; set; }
+        public EmployerCommitments TransferEmployerCommitments { get; set; }
+
+        public AccountEstimationProjectionCommitments(EmployerCommitments virtualEmployerCommitments, EmployerCommitments transferEmployerCommitments)
+        {
+            VirtualEmployerCommitments = virtualEmployerCommitments;
+            TransferEmployerCommitments = transferEmployerCommitments;
+        }
+    }
+}
