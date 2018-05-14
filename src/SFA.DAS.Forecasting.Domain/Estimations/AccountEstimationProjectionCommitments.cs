459b085fa42a83659aba294b0d4291651ce8e65c
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-05-14 14:44:45 +0100

    Change AccountEstimationProjectionCommitments
    Changed the AccountEstimationProjectionCommitments to use the
    projections created for actual commitments.

--- src/SFA.DAS.Forecasting.Domain/Estimations/AccountEstimationProjectionCommitments.cs
+++ src/SFA.DAS.Forecasting.Domain/Estimations/AccountEstimationProjectionCommitments.cs
@@ -1,16 +1,18 @@
-﻿using SFA.DAS.Forecasting.Domain.Commitments;
+﻿using System.Collections.Generic;
+using SFA.DAS.Forecasting.Domain.Commitments;
+using SFA.DAS.Forecasting.Models.Projections;
 
 namespace SFA.DAS.Forecasting.Domain.Estimations
 {
     public class AccountEstimationProjectionCommitments
     {
         public EmployerCommitments VirtualEmployerCommitments { get; set; }
-        public EmployerCommitments TransferEmployerCommitments { get; set; }
+        public IList<AccountProjectionModel> ActualAccountProjections { get; set; }
 
-        public AccountEstimationProjectionCommitments(EmployerCommitments virtualEmployerCommitments, EmployerCommitments transferEmployerCommitments)
+        public AccountEstimationProjectionCommitments(EmployerCommitments virtualEmployerCommitments, IList<AccountProjectionModel> actualAccountProjections)
         {
             VirtualEmployerCommitments = virtualEmployerCommitments;
-            TransferEmployerCommitments = transferEmployerCommitments;
+            ActualAccountProjections = actualAccountProjections;
         }
     }
 }
