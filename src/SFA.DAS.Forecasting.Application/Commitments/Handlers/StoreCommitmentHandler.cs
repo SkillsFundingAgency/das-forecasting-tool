7e2e3870f9313a93c0988534e4b7f9b33ecadc8f
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-05-22 12:06:19 +0100

    Change StoreCommitmentHandler to use Mapper
    Changed the StoreCommitmentHandler to use the PaymentMapper for mapping
    the PaymentCreateMessage to a Commitment.

--- src/SFA.DAS.Forecasting.Application/Commitments/Handlers/StoreCommitmentHandler.cs
+++ src/SFA.DAS.Forecasting.Application/Commitments/Handlers/StoreCommitmentHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using SFA.DAS.Forecasting.Application.Payments.Mapping;
 using SFA.DAS.Forecasting.Application.Payments.Messages;
 using SFA.DAS.Forecasting.Domain.Commitments;
 using SFA.DAS.Forecasting.Models.Commitments;
@@ -9,11 +10,13 @@ namespace SFA.DAS.Forecasting.Application.Commitments.Handlers
 {
     public class StoreCommitmentHandler
     {
+        private readonly IPaymentMapper _paymentMapper;
         private readonly IEmployerCommitmentRepository _repository;
         private readonly ILog _logger;
 
-        public StoreCommitmentHandler(IEmployerCommitmentRepository repository, ILog logger)
+        public StoreCommitmentHandler(IEmployerCommitmentRepository repository, ILog logger, IPaymentMapper paymentMapper)
         {
+            _paymentMapper = paymentMapper;
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
@@ -24,33 +27,14 @@ namespace SFA.DAS.Forecasting.Application.Commitments.Handlers
                 throw new InvalidOperationException($"Invalid payment created message. Earning details is null so cannot create commitment data. Employer account: {message.EmployerAccountId}, payment id: {message.Id}");
 
             var employerCommitment = await _repository.Get(message.EmployerAccountId, message.ApprenticeshipId);
-            
-            employerCommitment.RegisterCommitment(Map(message));
+
+            var commitmentModel = _paymentMapper.MapToCommitment(message);
+            employerCommitment.RegisterCommitment(commitmentModel);
 
             _logger.Debug($"Now storing the employer commitment. Employer: {message.EmployerAccountId}, ApprenticeshipId: {message.Id}");
             await _repository.Store(employerCommitment);
             _logger.Info($"Finished adding the employer commitment. Employer: {message.EmployerAccountId}, ApprenticeshipId: {message.Id}");
         }
 
-        private CommitmentModel Map(PaymentCreatedMessage message)
-        {
-            return new CommitmentModel
-            {
-                ApprenticeName = message.ApprenticeName,
-                SendingEmployerAccountId = message.SendingEmployerAccountId,
-                FundingSource = message.FundingSource,
-                LearnerId = message.ApprenticeshipId,
-                CourseLevel = message.CourseLevel,
-                CourseName = message.CourseName,
-                ProviderId = message.Ukprn,
-                ProviderName = message.ProviderName,
-                StartDate = message.EarningDetails.StartDate,
-                PlannedEndDate = message.EarningDetails.PlannedEndDate,
-                ActualEndDate = message.EarningDetails.ActualEndDate,
-                MonthlyInstallment = message.EarningDetails.MonthlyInstallment,
-                CompletionAmount = message.EarningDetails.CompletionAmount,
-                NumberOfInstallments = (short)message.EarningDetails.TotalInstallments
-            };
-        }
     }
 }
