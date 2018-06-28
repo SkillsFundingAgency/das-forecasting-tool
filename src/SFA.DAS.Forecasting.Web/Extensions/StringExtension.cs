d2000e506732885cdbd3e69a6691c0663da722a7
froberg <christofer.froberg@gmail.com>
2018-06-28 15:30:56 +0100

    CI-991, CI-910
    * Edit page total funding can't go over total funding cap fix
    * Edit page total funding correct value when loading page fix

---
+++ src/SFA.DAS.Forecasting.Web/Extensions/StringExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SFA.DAS.Forecasting.Web.Extensions
+{
+    public static class StringExtension
+    {
+        public static decimal ToDecimal(this string str)
+        {
+            var text = Regex.Replace(str ?? string.Empty, "[^0-9]", "");
+            if(decimal.TryParse(text, out decimal d))
+                return d;
+            return 0M;
+        }
+    }
+}
