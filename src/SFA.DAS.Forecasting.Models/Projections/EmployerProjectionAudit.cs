1fe7cd52d15d669decc5b9de5c8be0a6d93e6d79
abroodie <abroodie@outlook.com>
2018-06-28 16:40:28 +0100

    added EmployerProjectionAuditService to stop multiple threads starting a proejcction

---
+++ src/SFA.DAS.Forecasting.Models/Projections/EmployerProjectionAudit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SFA.DAS.Forecasting.Models.Projections
+{
+    public class EmployerProjectionAudit: IDocument
+    {
+        public string Id { get; set; }
+        public long EmployerAccountId { get; set; }
+        public DateTimeOffset LastGenerated { get; set; }
+    }
+}
