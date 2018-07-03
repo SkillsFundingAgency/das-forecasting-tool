a2d38689938df131aff4e0559180b925aca02a86
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-07-03 12:38:32 +0100

    Add EmployerCommitmentsModel
    Added the EmployerCommitmentsModel for storing commitments by type

---
+++ src/SFA.DAS.Forecasting.Models/Commitments/EmployerCommitmentsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Forecasting.Models.Commitments
+{
+    public class EmployerCommitmentsModel
+    {
+        public List<CommitmentModel> LevyFundedCommitments { get; set; }
+        public List<CommitmentModel> ReceivingEmployerTransferCommitments { get; set; }
+        public List<CommitmentModel> SendingEmployerTransferCommitments { get; set; }
+
+        public EmployerCommitmentsModel()
+        {
+            LevyFundedCommitments = new List<CommitmentModel>();
+            ReceivingEmployerTransferCommitments = new List<CommitmentModel>();
+            SendingEmployerTransferCommitments = new List<CommitmentModel>();
+        }
+    }
+}
