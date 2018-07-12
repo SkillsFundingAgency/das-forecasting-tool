4fdca9318fe5aa8079a2431c46d17000da184aef
froberg <christofer.froberg@gmail.com>
2018-07-12 11:48:10 +0100

    CI-919 -> check schema before alter

--- src/SFA.DAS.Forecasting.Database/PreDeployment/Script.Fix_AccountProjection_ProjectionGenerationType_Type.sql
+++ src/SFA.DAS.Forecasting.Database/PreDeployment/Script.Fix_AccountProjection_ProjectionGenerationType_Type.sql
@@ -10,37 +10,93 @@
 --------------------------------------------------------------------------------------
 */
 
-ALTER TABLE [dbo].[Balance] ALTER COLUMN Amount decimal(18,5) NOT NULL
-ALTER TABLE [dbo].[Balance] ALTER COLUMN RemainingTransferBalance  decimal(18,5) NOT NULL
-ALTER TABLE [dbo].[Balance] ALTER COLUMN TransferAllowance  decimal(18,5) NOT NULL
-ALTER TABLE [dbo].[Balance] ALTER COLUMN UnallocatedCompletionPayments decimal(18,5) NOT NULL
 
-ALTER TABLE [dbo].[Balance] ALTER COLUMN Amount decimal(18,5) NOT NULL
-ALTER TABLE [dbo].[Balance] ALTER COLUMN RemainingTransferBalance  decimal(18,5) NOT NULL
-ALTER TABLE [dbo].[Balance] ALTER COLUMN TransferAllowance  decimal(18,5) NOT NULL
-ALTER TABLE [dbo].[Balance] ALTER COLUMN UnallocatedCompletionPayments decimal(18,5) NOT NULL
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'Balance' and COLUMN_NAME = 'Amount' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[Balance] ALTER COLUMN Amount decimal(18,5) NOT NULL   
+END
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'Balance' and COLUMN_NAME = 'RemainingTransferBalance' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[Balance] ALTER COLUMN RemainingTransferBalance decimal(18,5) NOT NULL
+END
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'Balance' and COLUMN_NAME = 'TransferAllowance' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[Balance] ALTER COLUMN TransferAllowance decimal(18,5) NOT NULL
+END
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'Balance' and COLUMN_NAME = 'UnallocatedCompletionPayments' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[Balance] ALTER COLUMN UnallocatedCompletionPayments decimal(18,5) NOT NULL
+END
+
+-- AccountProjection
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'AccountProjection' and COLUMN_NAME = 'FundsIn' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN FundsIn decimal(18,5) NOT NULL
+END
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'AccountProjection' and COLUMN_NAME = 'TotalCostOfTraining' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN TotalCostOfTraining decimal(18,5) NOT NULL
+END
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'AccountProjection' and COLUMN_NAME = 'TransferOutTotalCostOfTraining' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN TransferOutTotalCostOfTraining decimal(18,5) NOT NULL
+END
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'AccountProjection' and COLUMN_NAME = 'TransferInTotalCostOfTraining' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN TransferInTotalCostOfTraining decimal(18,5) NOT NULL
+END
+
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'AccountProjection' and COLUMN_NAME = 'TransferInCompletionPayments' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN TransferInCompletionPayments decimal(18,5) NOT NULL
+END
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'AccountProjection' and COLUMN_NAME = 'CompletionPayments' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN CompletionPayments decimal(18,5) NOT NULL
+END
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'AccountProjection' and COLUMN_NAME = 'TransferOutCompletionPayments' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN TransferOutCompletionPayments decimal(18,5) NOT NULL
+END
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'AccountProjection' and COLUMN_NAME = 'FutureFunds' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN FutureFunds decimal(18,5) NOT NULL
+END
+
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'AccountProjection' and COLUMN_NAME = 'CoInvestmentEmployer' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN CoInvestmentEmployer decimal(18,5) NOT NULL
+END
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'AccountProjection' and COLUMN_NAME = 'CoInvestmentGovernment' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN CoInvestmentGovernment decimal(18,5) NOT NULL
+END
 
 
-ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN FundsIn decimal(18,5) NOT NULL
-ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN TotalCostOfTraining decimal(18,5) NOT NULL
-ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN TransferOutTotalCostOfTraining decimal(18,5) NOT NULL
-ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN TransferInTotalCostOfTraining decimal(18,5) NOT NULL
-ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN TransferInCompletionPayments decimal(18,5) NOT NULL
-ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN CompletionPayments decimal(18,5) NOT NULL
-ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN TransferOutCompletionPayments decimal(18,5) NOT NULL
-ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN FutureFunds decimal(18,5) NOT NULL
-ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN CoInvestmentEmployer decimal(18,5) NOT NULL
-ALTER TABLE [dbo].[AccountProjection] ALTER COLUMN CoInvestmentGovernment decimal(18,5) NOT NULL
+-- LevyDeclaration
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'LevyDeclaration' and COLUMN_NAME = 'LevyAmountDeclared' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[LevyDeclaration] ALTER COLUMN LevyAmountDeclared decimal(18,5) NOT NULL
+END
+
+-- Payment
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'Payment' and COLUMN_NAME = 'Amount' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[Payment] ALTER COLUMN Amount decimal(18,5) NOT NULL
+END
+
+-- Commitment
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'Commitment' and COLUMN_NAME = 'CompletionAmount' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[Commitment] ALTER COLUMN CompletionAmount decimal(18,5) NOT NULL
+END
+IF EXISTS(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'Commitment' and COLUMN_NAME = 'MonthlyInstallment' and NUMERIC_SCALE = 2)
+BEGIN
+	ALTER TABLE [dbo].[Commitment] ALTER COLUMN MonthlyInstallment decimal(18,5) NOT NULL
+END
 
 
 
-ALTER TABLE [dbo].[LevyDeclaration] ALTER COLUMN LevyAmountDeclared decimal(18,5) NOT NULL
-
-ALTER TABLE [dbo].[Payment] ALTER COLUMN Amount decimal(18,5) NOT NULL
-
-
-ALTER TABLE [dbo].[Commitment] ALTER COLUMN CompletionAmount decimal(18,5) NOT NULL
-ALTER TABLE [dbo].[Commitment] ALTER COLUMN MonthlyInstallment decimal(18,5) NOT NULL
 
 --BEGIN TRANSACTION
 --	BEGIN TRY
