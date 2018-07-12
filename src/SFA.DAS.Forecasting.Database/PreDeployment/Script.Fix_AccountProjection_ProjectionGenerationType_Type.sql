6850f89aff6c639809e365c89266761a51f47cff
froberg <christofer.froberg@gmail.com>
2018-07-12 09:58:33 +0100

    CI-919 -> readded old preload things..)

--- src/SFA.DAS.Forecasting.Database/PreDeployment/Script.Fix_AccountProjection_ProjectionGenerationType_Type.sql
+++ src/SFA.DAS.Forecasting.Database/PreDeployment/Script.Fix_AccountProjection_ProjectionGenerationType_Type.sql
@@ -41,3 +41,24 @@ ALTER TABLE [dbo].[Payment] ALTER COLUMN Amount decimal(18,5) NOT NULL
 
 ALTER TABLE [dbo].[Commitment] ALTER COLUMN CompletionAmount decimal(18,5) NOT NULL
 ALTER TABLE [dbo].[Commitment] ALTER COLUMN MonthlyInstallment decimal(18,5) NOT NULL
+
+--BEGIN TRANSACTION
+--	BEGIN TRY
+
+	if Exists (select * from sys.columns where object_id = OBJECT_ID('dbo.AccountProjection') and name = 'ProjectionGenerationType' and system_type_id = 52)
+	begin 
+
+		alter table AccountProjection add ProjectionGenerationType_tmp tinyint null;
+		Exec ('update AccountProjection set ProjectionGenerationType_tmp = ProjectionGenerationType');
+		alter table AccountProjection drop column ProjectionGenerationType;
+		EXEC sp_rename 'dbo.AccountProjection.ProjectionGenerationType_tmp', 'ProjectionGenerationType', 'COLUMN';
+		alter table AccountProjection alter column ProjectionGenerationType tinyint not null;
+
+	end
+
+ --   COMMIT
+ --   END TRY
+ --   BEGIN CATCH
+ --       ROLLBACK
+ --       --THROW; -- Only if you want reraise an exception (to determine the reason of the exception)
+ --   END CATCH 
