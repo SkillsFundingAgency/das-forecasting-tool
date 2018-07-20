d1e051ad9911ca3850b30c19137a7a72d92e2ab0
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-07-20 15:03:07 +0100

    Re-add validation around completion amount
    Added validation to handler so that we dont add commitments that dont
    have a completion amount

--- src/SFA.DAS.Forecasting.Application/Commitments/Handlers/StoreCommitmentHandler.cs
+++ src/SFA.DAS.Forecasting.Application/Commitments/Handlers/StoreCommitmentHandler.cs
@@ -27,7 +27,10 @@ namespace SFA.DAS.Forecasting.Application.Commitments.Handlers
 
 			var commitmentModel = _paymentMapper.MapToCommitment(message);
             
-            await _repository.Upsert(commitmentModel);
+            if(commitmentModel.CompletionAmount > 1)
+            {
+                await _repository.Upsert(commitmentModel);
+            }
 
             _logger.Info($"Finished adding the employer commitment. Employer: {message.EmployerAccountId}, ApprenticeshipId: {message.Id}");
         }
