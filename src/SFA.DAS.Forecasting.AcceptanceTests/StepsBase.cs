c908c797efa13923344b7ef7f6cc73afe9d489b5
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-07-20 10:52:03 +0100

    Update upsert for CommitmentsDataService
    Updated the upsert command to match on EmployerAccountId and LearnerId
    instead of the Id of the table. Also modified the update only to change
    the ActualEndate, Apprentices name and id as these are the only values
    that should be allowed to be updated on a commitment.

--- src/SFA.DAS.Forecasting.AcceptanceTests/StepsBase.cs
+++ src/SFA.DAS.Forecasting.AcceptanceTests/StepsBase.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Reflection;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Transactions;
 using Dapper;
 using Newtonsoft.Json;
@@ -14,6 +15,7 @@ using NUnit.Framework;
 using SFA.DAS.Forecasting.AcceptanceTests.Infrastructure;
 using SFA.DAS.Forecasting.AcceptanceTests.Levy;
 using SFA.DAS.Forecasting.AcceptanceTests.Payments;
+using SFA.DAS.Forecasting.Application.Commitments.Services;
 using SFA.DAS.Forecasting.Application.Shared;
 using SFA.DAS.Forecasting.Application.Shared.Services;
 using SFA.DAS.Forecasting.Data;
@@ -41,6 +43,8 @@ namespace SFA.DAS.Forecasting.AcceptanceTests
         protected IContainer NestedContainer { get => Get<IContainer>(); set => Set(value); }
         protected IDbConnection Connection => NestedContainer.GetInstance<IDbConnection>();
         protected ForecastingDataContext DataContext => NestedContainer.GetInstance<ForecastingDataContext>();
+
+        protected CommitmentsDataService CommitmentsDataService =>NestedContainer.GetInstance<CommitmentsDataService>();
         protected string EmployerHash { get => Get<string>("employer_hash"); set => Set(value, "employer_hash"); }
         protected static List<Process> Processes = new List<Process>();
         protected int EmployerAccountId => Config.EmployerAccountId;
@@ -276,7 +280,7 @@ namespace SFA.DAS.Forecasting.AcceptanceTests
                 var isTransferSender = CommitmentType == CommitmentType.TransferSender;
                 var isFundingSourceLevy = commitment.FundingSource.HasValue && commitment.FundingSource == FundingSource.Levy;
 
-                DataContext.Commitments.Add(new CommitmentModel
+                var commitmentModel = new CommitmentModel
                 {
                     EmployerAccountId = isTransferSender && isFundingSourceLevy ? EmployerAccountId : receiverId,
                     LearnerId = i + 1,
@@ -296,7 +300,8 @@ namespace SFA.DAS.Forecasting.AcceptanceTests
                     FundingSource = CommitmentType == CommitmentType.LevyFunded 
                         ? FundingSource.Levy 
                         : commitment.FundingSource ?? FundingSource.Transfer
-                });
+                };
+                Task.FromResult(CommitmentsDataService.Upsert(commitmentModel)).Wait();
             }
 
             DataContext.SaveChanges();
