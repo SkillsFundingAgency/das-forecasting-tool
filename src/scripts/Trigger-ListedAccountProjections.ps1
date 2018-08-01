5d739efc1cf2620cb1f448e7c100c2962dd2ebf7
abroodie <abroodie@outlook.com>
2018-08-01 15:00:31 +0100

    updated Trigger projection scripts to set the start period

--- src/scripts/Trigger-ListedAccountProjections.ps1
+++ src/scripts/Trigger-ListedAccountProjections.ps1
@@ -7,15 +7,15 @@
   #************ Projection source can be either PaymentPeriodEnd or LevyDeclaration
 
  #*************************************************************
- $accountIds = @(12345,
- 54321, 
- 67890,
- 09876
+ $accountIds = @(12345
+ #, 54321
+ #,67890
+ #,09876
  )
  #*************************************************************
 
  foreach ($listedAccountId in $accountIds) {    
-    .\Trigger-AccountProjection.ps1 -connectionString $connectionString -accountId $listedAccountId -projectionSource $projectionSource
+    .\Trigger-AccountProjection.ps1 -connectionString $connectionString -accountId $listedAccountId -projectionSource $projectionSource -startPeriodMonth 7 -startPeriodYear 2018
  }
 
  
