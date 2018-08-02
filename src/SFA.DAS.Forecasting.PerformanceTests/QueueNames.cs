054310f9b0f1a4551c77398a2ead83c9e1431ff2
abroodie <abroodie@outlook.com>
2018-08-02 23:12:40 +0100

    now tracks durations for commitments, balance & payments

---
+++ src/SFA.DAS.Forecasting.PerformanceTests/QueueNames.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.Forecasting.PerformanceTests
+{
+    public static class QueueNames
+    {
+        public const string LevyValidateDeclaration = "forecasting-levy-validate-declaration";
+    }
+}
