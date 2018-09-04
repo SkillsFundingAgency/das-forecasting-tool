0d0187fac3a0e1560521c17de4094a2d10116968
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-09-04 10:16:33 +0100

    WIP - TimeTrigger for Refresh apprentices

---
+++ src/SFA.DAS.Forecasting.Apprenticeship.Functions/RefreshApprenticeshipsForEmployer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using SFA.DAS.Forecasting.Messages.ApprenticeshipCourses;
+
+namespace SFA.DAS.Forecasting.Apprenticeship.Functions
+{
+    [StorageAccount("StorageConnectionString")]
+    public class RefreshApprenticeshipsForEmployer
+    {
+        [FunctionName("RefreshApprenticeshipCoursesFunction")]
+        [return: Queue(QueueNames.GetApprenticeshipsForEmployer)]
+        public static string Run([TimerTrigger("0 0 0 */1 * *")]TimerInfo myTimer, TraceWriter log)
+        {
+            log.Verbose("Triggering scheduled run of refresh apprenticeships for employers.");
+            return "";
+        }
+    }
+}
