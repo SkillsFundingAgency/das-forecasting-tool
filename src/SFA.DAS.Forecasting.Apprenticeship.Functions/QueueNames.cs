03d49fde768d1ca54ba5a18870bb91760cd7d924
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-09-05 16:43:06 +0100

    Update queue name to match the functionality

--- src/SFA.DAS.Forecasting.Apprenticeship.Functions/QueueNames.cs
+++ src/SFA.DAS.Forecasting.Apprenticeship.Functions/QueueNames.cs
@@ -2,8 +2,8 @@
 {
     internal class QueueNames
     {
-        public const string GetEmployersForApprenticeshipUpdate = "forecasting-get-employer-for-apprenticeship-update";
-        public const string GetApprenticeshipsForEmployer = "forecasting-get-apprenticeship-for-employer";
+        public const string RefreshEmployersForApprenticeshipUpdate = "forecasting-refresh-employer-for-apprenticeship-update";
+        public const string RefreshApprenticeshipsForEmployer = "forecasting-refresh-apprenticeship-for-employer";
         public const string StoreApprenticeships = "forecasting-store-apprenticeship-for-employer";
     }
 }
