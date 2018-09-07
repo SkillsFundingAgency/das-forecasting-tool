9fa28e59aba31132cf6975e8f6377b887ca5cf1e
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-09-07 10:42:37 +0100

    Update EmployerCommitmentRepository
    Updated the EmployerCommitmentRepository to have Get and Store methods
    exposed for EmployerCommitment

--- src/SFA.DAS.Forecasting.Domain/Commitments/EmployerCommitmentRepository.cs
+++ src/SFA.DAS.Forecasting.Domain/Commitments/EmployerCommitmentRepository.cs
@@ -8,6 +8,8 @@ namespace SFA.DAS.Forecasting.Domain.Commitments
     public interface IEmployerCommitmentRepository
     {
         Task Upsert(CommitmentModel commitment);
+        Task<EmployerCommitment> Get(long employerAccountId, long apprenticeshipId);
+        Task Store(EmployerCommitment commitment);
     }
 
     public class EmployerCommitmentRepository : IEmployerCommitmentRepository
@@ -22,6 +24,22 @@ namespace SFA.DAS.Forecasting.Domain.Commitments
         public async Task Upsert(CommitmentModel commitment)
         {
             await _dataService.Upsert(commitment);
+        }
+
+        public async Task<EmployerCommitment> Get(long employerAccountId, long apprenticeshipId)
+        {
+            var commitment = await _dataService.Get(employerAccountId, apprenticeshipId)
+                             ?? new CommitmentModel
+                             {
+                                 EmployerAccountId = employerAccountId,
+                                 ApprenticeshipId = apprenticeshipId
+                             };
+            return new EmployerCommitment(commitment);
+        }
+
+        public async Task Store(EmployerCommitment commitment)
+        {
+            await _dataService.Store(commitment.Commitment);
         }
     }
 }
