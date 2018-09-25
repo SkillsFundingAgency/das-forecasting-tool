54d9b92024d8c204b26a0160923d30354f0b3f2a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-09-25 10:50:07 +0100

    Add stub for new function for devops config

---
+++ src/SFA.DAS.Forecasting.Commitments.Functions/QueueNames.cs
@@ -0,0 +1,11 @@
+﻿namespace SFA.DAS.Forecasting.Commitments.Functions
+{
+    internal class QueueNames
+    {
+        public const string RefreshEmployersForApprenticeshipUpdate = "forecasting-refresh-employer-for-apprenticeship-update";
+        public const string RefreshApprenticeshipsForEmployer = "forecasting-refresh-apprenticeship-for-employer";
+        public const string StoreApprenticeships = "forecasting-store-apprenticeship-for-employer";
+        public const string AllowProjection = "forecasting-payment-allow-projection";
+
+    }
+}
---
+++ src/SFA.DAS.Forecasting.Commitments.Functions/RefreshApprenticeshipsForEmployer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace SFA.DAS.Forecasting.Commitments.Functions
+{
+    [StorageAccount("StorageConnectionString")]
+    public class RefreshApprenticeshipsForEmployer
+    {
+        [FunctionName("RefreshApprenticeshipCoursesFunction")]
+        [return: Queue(QueueNames.RefreshEmployersForApprenticeshipUpdate)]
+        public static string Run([TimerTrigger("0 0 0 */1 * *")]TimerInfo myTimer, TraceWriter log)
+        {
+            log.Verbose("Triggering scheduled run of refresh apprenticeships for employers.");
+            return "RefreshApprenticeshipsForEmployer";
+        }
+    }
+}
