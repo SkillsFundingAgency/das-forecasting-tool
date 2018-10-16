39e6b5cc6942f2cf077e723edb5e187de9ba72fa
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-10-16 11:29:07 +0100

    Update structure map registration
    Updated the structure map registration to seperate the registration of
    components for the web site and for the functions. New
    DefaultWebRegistry class created

---
+++ src/SFA.DAS.Forecasting.Application/Infrastructure/Registries/DefaultWebRegistry.cs
@@ -0,0 +1,32 @@
+﻿using SFA.DAS.Forecasting.Application.Balance.Services;
+using SFA.DAS.Forecasting.Core;
+using SFA.DAS.Forecasting.Data;
+using SFA.DAS.Forecasting.Domain.Balance.Services;
+using StructureMap;
+
+namespace SFA.DAS.Forecasting.Application.Infrastructure.Registries
+{
+    public class DefaultWebRegistry : Registry
+    {
+        public DefaultWebRegistry()
+        {
+
+            For<IForecastingDataContext>()
+                .Use<ForecastingDataContext>()
+                .Ctor<IApplicationConnectionStrings>("config")
+                .Is(ctx => ctx.GetInstance<IApplicationConnectionStrings>())
+                .ContainerScoped();
+
+            if (ConfigurationHelper.IsDevOrAtEnvironment)
+            {
+                For<IAccountBalanceService>()
+                    .Use<DevAccountBalanceService>();
+            }
+            else
+            {
+                For<IAccountBalanceService>()
+                    .Use<AccountBalanceService>();
+            }
+        }
+    }
+}
