96f607374be0836cc57522973e536bccce29196b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-10-22 20:52:25 +0100

    Remove message types from Function

--- src/SFA.DAS.Forecasting.PreLoad.Functions/Models/PreLoadAllEmployersRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SFA.DAS.Forecasting.PreLoad.Functions.Models
-{
-    public class PreLoadAllEmployersRequest
-    {
-        public string PeriodYear { get; set; }
-
-        public short PeriodMonth { get; set; }
-    }
-}
