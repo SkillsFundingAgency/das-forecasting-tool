d0a49fd8595725e07f619b55810497a141d4a6ae
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-10-23 14:38:59 +0100

    Add preload levy request message

---
+++ src/SFA.DAS.Forecasting.Application/Levy/Messages/PreLoad/PreLoadLevyRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace SFA.DAS.Forecasting.Application.Levy.Messages.PreLoad
+{
+    public class PreLoadLevyRequest
+    {
+        public long[] EmployerAccountIds { get; set; }
+        public string PeriodYear { get; set; }
+        public short PeriodMonth { get; set; }
+        public long SubstitutionId { get; set; }
+    }
+}
