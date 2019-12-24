6d4e372fcb007f890d5a4908f1656cb106846ea0
vas <vasantha.kasirajan@digital.education.gov.uk>
2019-12-24 10:51:38 +0000

    CON-1188- Contextual Support widget - Forecasting

--- src/SFA.DAS.Forecasting.Web/Extensions/HtmlHelperExtensions.cs
+++ src/SFA.DAS.Forecasting.Web/Extensions/HtmlHelperExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Linq.Expressions;
 using System.Web.Mvc;
 
@@ -20,6 +21,44 @@ namespace SFA.DAS.Forecasting.Web.Extensions
             }
 
             return new MvcHtmlString("error");
-        }   
+        }
+
+
+        public static MvcHtmlString SetZenDeskLabels(this HtmlHelper html, params string[] labels)
+        {
+            var apiCallString =
+                "<script type=\"text/javascript\">zE('webWidget', 'helpCenter:setSuggestions', { labels: [";
+
+            var first = true;
+            foreach (var label in labels)
+            {
+                if (!string.IsNullOrEmpty(label))
+                {
+                    if (!first) apiCallString += ",";
+                    first = false;
+
+                    apiCallString += $"'{ EscapeApostrophes(label) }'";
+                }
+            }
+
+            apiCallString += "] });</script>";
+
+            return MvcHtmlString.Create(apiCallString);
+        }
+
+        private static string EscapeApostrophes(string input)
+        {
+            return input.Replace("'", @"\'");
+        }
+
+        public static string GetZenDeskSnippetKey(this HtmlHelper html)
+        {
+            return ConfigurationManager.AppSettings["ZenDeskSnippetKey"];
+        }
+
+        public static string GetZenDeskSnippetSectionId(this HtmlHelper html)
+        {
+            return ConfigurationManager.AppSettings["ZenDeskSectionId"];            
+        }
     }
 }
