2a26a3606446b853e93eafea4193c9eb5bf09d96
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2020-04-03 11:22:47 +0100

    CON-1538-fix rename

--- src/SFA.DAS.Forecasting.Web/Extensions/ClaimsPrincipalExtensions.cs
+++ src/SFA.DAS.Forecasting.Web/Extensions/ClaimsPrincipalExtensions.cs
@@ -11,26 +11,26 @@ namespace SFA.DAS.Forecasting.Web.Extensions
     {
         public static string GetDisplayName(this ClaimsPrincipal user)
         {
-            return user.FindFirst(EmployerRecruitClaims
+            return user.FindFirst(ForecastingClaims
                 .IdamsUserDisplayNameClaimTypeIdentifier).Value;
         }
 
         public static string GetEmailAddress(this ClaimsPrincipal user)
         {
-            return user.FindFirst(EmployerRecruitClaims
+            return user.FindFirst(ForecastingClaims
                 .IdamsUserEmailClaimTypeIdentifier).Value;
         }
 
         public static string GetUserId(this ClaimsPrincipal user)
         {
-            return user.FindFirst(EmployerRecruitClaims
+            return user.FindFirst(ForecastingClaims
                 .IdamsUserIdClaimTypeIdentifier).Value;
         }
 
         public static IEnumerable<string> GetEmployerAccounts(this ClaimsPrincipal user)
         {
             var employerAccountClaim = user.FindFirst(c =>
-                c.Type.Equals(EmployerRecruitClaims.AccountsClaimsTypeIdentifier));
+                c.Type.Equals(ForecastingClaims.AccountsClaimsTypeIdentifier));
 
             if (string.IsNullOrEmpty(employerAccountClaim?.Value))
                 return Enumerable.Empty<string>();
