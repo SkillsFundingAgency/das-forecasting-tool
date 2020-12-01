cea0fe3a1506e499d4fa7002c3881f9ab2cad2a3
vas <vasantha.kasirajan@digital.education.gov.uk>
2020-12-01 08:50:56 +0000

    CON-2670:Sql script to get the Completed & Stopped apprenticeship

---
+++ src/SFA.DAS.Forecasting.Database/AdhocScripts/Manual/CON-2670-stopped-apprenticeship.sql
@@ -0,0 +1,64 @@
+/*
+Script to populate Commitment data for FPT  from Commitments(Approvals) source data
+
+Instructions for use:
+1. Turn on SQL CMD mode and Results to Text and max characters in text output to 8192 (query->query options->results->text->Maximum number of characters displayed in each column)
+2. Execute this script against Commitments database
+3. Execute the resulting script against Forecasting database
+
+Expectations:
+~? minutes on commitments database
+~? minutes on forecasting db
+*/
+
+SET NOCOUNT ON
+
+declare @MAXINSERT int = 1000
+
+--table var declarations
+print 'declare @TempStoppedCommitments table ([ApprenticeshipId] bigint, [StopDate] datetime, [PaymentStatus] smallint)'
+
+BEGIN TRY
+
+	--commitments
+	SELECT
+	CASE (ROW_NUMBER() OVER (ORDER BY a.Id) % @MAXINSERT) 
+	WHEN 1 
+	THEN 'insert into @TempStoppedCommitments ([ApprenticeshipId], [StopDate], [PaymentStatus]) values' + char(13) + char(10) else '' end +
+	' (' + convert(varchar,[Id]) +', ' + '''' + convert(varchar,[StopDate],121) + ''''+', ' +'''' + convert(varchar,PaymentStatus) + '''' + ')' + 	
+	CASE 
+	WHEN ((ROW_NUMBER() OVER (ORDER BY a.Id) % @MAXINSERT = 0)
+	OR (ROW_NUMBER() OVER (ORDER BY a.Id) = (select count(1) from dbo.Apprenticeship where PaymentStatus = 3 and a.StopDate is not null )))
+	THEN ''
+	ELSE ',' END
+	FROM  [dbo].[Apprenticeship] a
+	WHERE PaymentStatus = 3 and a.StopDate is not null	
+
+	
+	PRINT '	BEGIN TRANSACTION '
+
+	PRINT '	
+
+	UPDATE [dbo].[Commitment] 
+	SET ActualEndDate = TC.CompletionDate , Status = 1
+	FROM Commitment C 
+	INNER JOIN
+	@TempStoppedCommitments TC 
+	ON C.ApprenticeshipId = TC.ApprenticeshipId AND TC.PaymentStatus = 3
+
+	print ''updated '' + convert(varchar,@@ROWCOUNT) + '' Stopped Commitments''
+	print ''Completed''
+
+	COMMIT TRANSACTION
+	'
+
+END TRY
+BEGIN CATCH
+	SELECT  
+		 ERROR_NUMBER() AS ErrorNumber  
+        ,ERROR_SEVERITY() AS ErrorSeverity  
+        ,ERROR_STATE() AS ErrorState  
+        ,ERROR_PROCEDURE() AS ErrorProcedure  
+        ,ERROR_LINE() AS ErrorLine  
+        ,ERROR_MESSAGE() AS ErrorMessage;    
+END CATCH
