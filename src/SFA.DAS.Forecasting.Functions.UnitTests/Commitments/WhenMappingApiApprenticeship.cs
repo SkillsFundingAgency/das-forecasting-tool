1dec2fe87e51a5a44f141110d5c4507110eb1017
Corey <corey.faulconbridge@digital.education.gov.uk>
2021-04-21 12:06:00 +0100

    [CON-3409] Add unit test framework around area requiring change, consolidate some testing packages.

---
+++ src/SFA.DAS.Forecasting.Functions.UnitTests/Commitments/WhenMappingApiApprenticeship.cs
@@ -0,0 +1,41 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Forecasting.Application.Apprenticeship.Messages;
+using SFA.DAS.Forecasting.Application.ApprenticeshipCourses.Services;
+using SFA.DAS.Forecasting.Commitments.Functions.Application;
+using System.Threading.Tasks;
+using ApiApprenticeship = SFA.DAS.Commitments.Api.Types.Apprenticeship.Apprenticeship;
+
+namespace SFA.DAS.Forecasting.Functions.UnitTests.Commitments
+{
+    [TestFixture]
+    public class WhenMappingApiApprenticeship
+    {
+        private Mock<IApprenticeshipCourseDataService> _apprenticeshipCourseDataServiceMock;
+        private Mapper _sut;
+        private IFixture _fixture = new Fixture();
+
+        [SetUp]
+        public void SetUp()
+        {
+            _apprenticeshipCourseDataServiceMock = new Mock<IApprenticeshipCourseDataService>();
+            _sut = new Mapper(_apprenticeshipCourseDataServiceMock.Object);
+        }
+
+        [Test]
+        public async Task IfNull_ReturnEmptyMessage()
+        {
+            // Arrange
+            var emptyResult = new ApprenticeshipMessage();
+            ApiApprenticeship apiApprenticeship = null;
+
+            // Act
+            var result = await _sut.Map(apiApprenticeship);
+
+            // Assert
+            result.Should().BeEquivalentTo(emptyResult);
+        }
+    }
+}
---
+++ src/SFA.DAS.Forecasting.Functions.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SFA.DAS.Forecasting.Functions.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("SFA.DAS.Forecasting.Functions.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("22464e6d-3a4f-48e2-9ff3-f582d5420170")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
