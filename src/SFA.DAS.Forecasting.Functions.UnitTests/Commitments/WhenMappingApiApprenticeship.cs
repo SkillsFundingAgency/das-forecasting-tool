384d65285cf27ed38074af72879c8f4236a0a288
Corey <corey.faulconbridge@digital.education.gov.uk>
2021-04-21 12:25:08 +0100

    more missing unit tests

--- src/SFA.DAS.Forecasting.Functions.UnitTests/Commitments/WhenMappingApiApprenticeship.cs
+++ src/SFA.DAS.Forecasting.Functions.UnitTests/Commitments/WhenMappingApiApprenticeship.cs
@@ -37,5 +37,59 @@ namespace SFA.DAS.Forecasting.Functions.UnitTests.Commitments
             // Assert
             result.Should().BeEquivalentTo(emptyResult);
         }
+
+        [Test]
+        public async Task NotNull_ShouldGetApprenticeshipTrainingCourse()
+        {
+            // Arrange
+            ApiApprenticeship apiApprenticeship = _fixture.Create<ApiApprenticeship>();
+
+            // Act
+            var result = await _sut.Map(apiApprenticeship);
+
+            // Assert
+            _apprenticeshipCourseDataServiceMock
+                .Verify(mock => mock.GetApprenticeshipCourse(It.Is<string>(x => x == apiApprenticeship.TrainingCode)), Times.Once);
+        }
+
+        [Test]
+        public async Task NotNull_ShouldMapToApprenticeshipMessage()
+        {
+            // Arrange
+            ApiApprenticeship apiApprenticeship = _fixture.Create<ApiApprenticeship>();
+
+            // Act
+            var result = await _sut.Map(apiApprenticeship);
+
+            // Assert
+            result.EmployerAccountId.Should().Be(apiApprenticeship.EmployerAccountId);
+            result.SendingEmployerAccountId.Should().Be(apiApprenticeship.TransferSenderId);
+            result.ProviderId.Should().Be(apiApprenticeship.ProviderId);
+            result.ProviderName.Should().Be(apiApprenticeship.ProviderName);
+            result.ApprenticeshipId.Should().Be(apiApprenticeship.Id);
+            result.CourseName.Should().Be(apiApprenticeship.TrainingName);
+            result.StartDate.Should().Be(apiApprenticeship.StartDate.Value);
+            result.PlannedEndDate.Should().Be(apiApprenticeship.EndDate.Value);
+            result.ActualEndDate.Should().Be(null);
+        }
+
+        [Test]
+        [TestCase(null, 0)]
+        [TestCase("string", 0)]
+        [TestCase("1234", 1234)]
+        public async Task NotNull_ShouldMapToApprenticeshipMessageLearnerId(string apprenticeshipUln, long expectedLearnerId)
+        {
+            // Arrange
+            ApiApprenticeship apiApprenticeship = _fixture
+                .Build<ApiApprenticeship>()
+                .With(x => x.ULN, apprenticeshipUln)
+                .Create();
+
+            // Act
+            var result = await _sut.Map(apiApprenticeship);
+
+            // Assert
+            result.LearnerId.Should().Be(expectedLearnerId);
+        }
     }
 }
