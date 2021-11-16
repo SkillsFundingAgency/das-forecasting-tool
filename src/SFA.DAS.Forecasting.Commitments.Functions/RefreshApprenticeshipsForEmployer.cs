d867786b6b02afad0fccffdd1f9f55d181fe193b
Shahzad <hkf-tech@users.noreply.github.com>
2021-11-16 23:57:59 +0000

    adjusting the timer

--- src/SFA.DAS.Forecasting.Commitments.Functions/RefreshApprenticeshipsForEmployer.cs
+++ src/SFA.DAS.Forecasting.Commitments.Functions/RefreshApprenticeshipsForEmployer.cs
@@ -8,7 +8,7 @@ namespace SFA.DAS.Forecasting.Commitments.Functions
     {
         [FunctionName("RefreshApprenticeshipCoursesFunction")]
         [return: Queue(QueueNames.RefreshEmployersForApprenticeshipUpdate)]
-        public static string Run([TimerTrigger("0 */2 * * * *")]TimerInfo myTimer, TraceWriter log)
+        public static string Run([TimerTrigger("0 0 0 */1 * *")]TimerInfo myTimer, TraceWriter log)
         {
             log.Verbose("Triggering scheduled run of refresh apprenticeships for employers.");
             return "RefreshApprenticeshipsForEmployer";
