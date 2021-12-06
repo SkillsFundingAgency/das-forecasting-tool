7ffb1fc29c287ba008fe555a35ebe5d54f753664
Shahzad <hkf-tech@users.noreply.github.com>
2021-12-06 08:13:37 +0000

    CON-4127 refactoring

--- src/SFA.DAS.Forecasting.Data/ForecastingDbConfiguration.cs
+++ src/SFA.DAS.Forecasting.Data/ForecastingDbConfiguration.cs
@@ -1,9 +1,6 @@
-﻿using System.Data.Common;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Data.Entity.SqlServer;
-using System.Data.SqlClient;
-using Microsoft.Azure.Services.AppAuthentication;
 using SFA.DAS.Forecasting.Core;
 
 namespace SFA.DAS.Forecasting.Data
@@ -30,36 +27,6 @@ namespace SFA.DAS.Forecasting.Data
         partial void DisposePartial(bool disposing)
         {
             Database?.Connection?.GetType()?.GetField("StateChange", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.SetValue(Database?.Connection, null);
-        }
-    }
-
-    public class CustomDbConnectionFactory : IDbConnectionFactory
-    {
-        private bool _isDevEnviornment;
-
-        public CustomDbConnectionFactory(bool isDevEnvironment)
-        {
-            _isDevEnviornment = isDevEnvironment;
-        }
-
-        private static string AzureResource = "https://database.windows.net/";
-
-        public DbConnection CreateConnection(string nameOrConnectionString)
-        {
-            if (_isDevEnviornment)
-            {
-                return new SqlConnection("Data Source=(localdb)\\projectsv13;Initial Catalog=SFA.DAS.Forecasting.Database;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True");
-            }
-            else
-            {
-                var azureServiceTokenProvider = new AzureServiceTokenProvider();
-                var accessToken = azureServiceTokenProvider.GetAccessTokenAsync(AzureResource).Result;
-                return new SqlConnection
-                {
-                    ConnectionString = nameOrConnectionString,
-                    AccessToken = accessToken,
-                };
-            }
         }
     }
 }
