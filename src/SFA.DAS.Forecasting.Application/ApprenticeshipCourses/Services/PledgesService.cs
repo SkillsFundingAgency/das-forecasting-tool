0bc30d2157d1673a72f094f4b45dcee0dbdc1883
chrisfoster76 <chrisfoster186@googlemail.com>
2022-03-11 12:37:04 +0000

    Adds basic infrastructure for pledge service and logging of total items to prove working

---
+++ src/SFA.DAS.Forecasting.Application/ApprenticeshipCourses/Services/PledgesService.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Forecasting.Application.Infrastructure.OuterApi;
+using SFA.DAS.Forecasting.Models.Pledges;
+
+namespace SFA.DAS.Forecasting.Application.ApprenticeshipCourses.Services
+{
+    public interface IPledgesService
+    {
+        Task<List<Pledge>> GetPledges();
+        Task<List<Models.Pledges.Application>> GetApplications();
+    }
+
+    public class PledgesService : IPledgesService
+    {
+        private readonly IApiClient _apiClient;
+        private readonly ILogger<PledgesService> _logger;
+
+        public PledgesService(IApiClient apiClient, ILogger<PledgesService> logger)
+        {
+            _apiClient = apiClient;
+            _logger = logger;
+        }
+
+        public async Task<List<Pledge>> GetPledges()
+        {
+            var response = await _apiClient.Get<GetPledgesResponse>(new GetPledgesApiRequest());
+            _logger.LogInformation($"LTM inner api reports {response.TotalPledges} total pledges");
+            return new List<Pledge>();
+        }
+
+        public async Task<List<Models.Pledges.Application>> GetApplications()
+        {
+            var response = await _apiClient.Get<GetApplicationsResponse>(new GetApplicationsApiRequest());
+            _logger.LogInformation($"LTM inner api reports {response.TotalApplications} total applications");
+            return new List<Models.Pledges.Application>();
+        }
+    }
+}
---
+++ src/SFA.DAS.Forecasting.Application/Infrastructure/OuterApi/GetPledgesApiRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.DAS.Forecasting.Application.Infrastructure.OuterApi
+{
+    public class GetPledgesApiRequest : IGetApiRequest
+    {
+        public string GetUrl => "pledges";
+    }
+
+    public class GetPledgesResponse
+    {
+        public int TotalPledges { get; set; }
+    }
+}
