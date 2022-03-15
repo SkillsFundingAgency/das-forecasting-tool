691352d147fbf7ac41018b67b67dcfa54872c34e
chrisfoster76 <chrisfoster186@googlemail.com>
2022-03-15 16:07:09 +0000

    Adds exception handling

--- src/SFA.DAS.Forecasting.Application/ApprenticeshipCourses/Services/PledgesService.cs
+++ src/SFA.DAS.Forecasting.Application/ApprenticeshipCourses/Services/PledgesService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.Forecasting.Application.Infrastructure.OuterApi;
@@ -25,9 +26,17 @@ namespace SFA.DAS.Forecasting.Application.ApprenticeshipCourses.Services
 
         public async Task<List<Pledge>> GetPledges()
         {
-            var response = await _apiClient.Get<GetPledgesResponse>(new GetPledgesApiRequest());
-            _logger.LogInformation($"LTM inner api reports {response.TotalPledges} total pledges");
-            return new List<Pledge>();
+            try
+            {
+                var response = await _apiClient.Get<GetPledgesResponse>(new GetPledgesApiRequest());
+                _logger.LogInformation($"LTM inner api reports {response.TotalPledges} total pledges");
+                return new List<Pledge>();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception occurred getting pledges");
+                throw;
+            }
         }
 
         public async Task<List<Models.Pledges.Application>> GetApplications()
