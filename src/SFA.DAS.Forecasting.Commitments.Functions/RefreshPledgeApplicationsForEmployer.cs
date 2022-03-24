851358a06a37901cbd18bd62bbbd50a4fb4eccbd
chrisfoster76 <chrisfoster186@googlemail.com>
2022-03-24 13:44:49 +0000

    Adds timer function to trigger pledge apps refresh

---
+++ src/SFA.DAS.Forecasting.Commitments.Functions/RefreshPledgeApplicationsForEmployer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace SFA.DAS.Forecasting.Commitments.Functions
+{
+    [StorageAccount("StorageConnectionString")]
+    public class RefreshPledgeApplicationsForEmployer
+    {
+        [FunctionName("RefreshApprenticeshipCoursesFunction")]
+        [return: Queue(QueueNames.RefreshEmployersForPledgeApplications)]
+        public static string Run([TimerTrigger("0 0 0 */1 * *")] TimerInfo myTimer, TraceWriter log)
+        {
+            log.Verbose("Triggering scheduled run of refresh pledge applications for employers.");
+            return "RefreshPledgeApplicationsForEmployer";
+        }
+    }
+}
