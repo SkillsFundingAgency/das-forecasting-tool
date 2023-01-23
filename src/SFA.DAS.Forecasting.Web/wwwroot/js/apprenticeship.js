f8d3114863d65b5232217cc92a0a95ed47f05a4f
James King <hello@jamesmichaelking.com>
2023-01-23 14:06:01 +0000

    JS update

--- src/SFA.DAS.Forecasting.Web/wwwroot/js/apprenticeship.js
+++ src/SFA.DAS.Forecasting.Web/wwwroot/js/apprenticeship.js
@@ -1,15 +1,68 @@
-function ApprenticeshipForm() {
+function ShowFundingEstimate() {
     this.apprenticeshipRoleSelectId = 'choose-apprenticeship'
     this.apprenticeshipRole = document.getElementById(this.apprenticeshipRoleSelectId)
+    this.apprenticeshipNumber = document.getElementById('no-of-app')
+    this.apprenticeshipDateMonth = document.getElementById('startDateMonth')
+    this.apprenticeshipDateYear = document.getElementById('startDateYear')
     this.selectedStandardId = 0
+    this.numberOfApprentices = 0
+    this.startMonth = 0
+    this.startYear = 0
+    this.monthsRemaining = 0
+    this.estimate = 0
+    this.showEstimate = false
 }
 
-ApprenticeshipForm.prototype.init = function() {
+ShowFundingEstimate.prototype.init = function() {
     this.autoComplete()
+    this.setupEvents()
+    this.pageLoad()
+    this.checkFieldValues()
+}
+
+ShowFundingEstimate.prototype.pageLoad = function () {
+    if (this.apprenticeshipRole.value !== "") {
+      this.selectedStandardId = this.apprenticeshipRole.value
+    }
+    if (this.apprenticeshipNumber.value !== "") {
+      this.numberOfApprentices = this.apprenticeshipNumber.value
+    }
+    if (this.apprenticeshipDateMonth.value !== "") {
+      this.startMonth = this.apprenticeshipDateMonth.value
+      this.dateChange()
+    }
+    if (this.apprenticeshipDateYear.value !== "") {
+      this.startYear = this.apprenticeshipDateYear.value
+      this.dateChange()
+    }
+  }
+
+
+ShowFundingEstimate.prototype.setupEvents = function () {
+    var that = this
+    this.apprenticeshipNumber.onkeyup = function () {
+        that.numberOfApprentices = this.value | 0
+        that.checkFieldValues()
+    }
+    this.apprenticeshipDateMonth.onkeyup = function () {
+        that.startMonth = this.value | 0
+        that.dateChange()
+        that.checkFieldValues()
+    }
+    this.apprenticeshipDateYear.onkeyup = function () {
+        that.startYear = this.value | 0
+        that.dateChange()
+        that.checkFieldValues()
+    }
+}
+
+ShowFundingEstimate.prototype.dateChange = function () {
+    var futureDate = new Date(this.startYear + ", " + this.startMonth + ", 1")
+    this.monthsRemaining = this.calculateMonthsDifference(futureDate);
 }
 
 
-ApprenticeshipForm.prototype.autoComplete = function() {
+ShowFundingEstimate.prototype.autoComplete = function() {
     var that = this
     accessibleAutocomplete.enhanceSelectElement({
         selectElement: that.apprenticeshipRole,
@@ -38,9 +91,73 @@ ApprenticeshipForm.prototype.autoComplete = function() {
                 this.selectElement.selectedIndex = 0;
                 that.selectedStandardId = 0
             }
+            that.checkFieldValues()
         }
     });
 }
 
-var apprenticeshipForm = new ApprenticeshipForm();
-apprenticeshipForm.init()
+ShowFundingEstimate.prototype.getEstimate = function () {
+    var that = this
+    var params = 'SelectedStandardId=' + this.selectedStandardId + '&NumberOfApprentices=' + this.numberOfApprentices + '&StartDate=' + this.startYear + '-' + this.startMonth + '-01'
+    var url = window.location.href.split('?')[0] + '/funding-estimate?' + params
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+        try {
+            var response = JSON.parse(xhr.responseText)
+            var estimate = parseInt(response.amount)
+            if (isNaN(estimate)) {
+            that.estimate = 0
+            that.showEstimate = false
+            } else {
+            that.estimate = estimate
+            that.hasEnoughFunding = response.hasEnoughFunding
+            that.showEstimate = true
+            }
+            that.updateUI()
+        } catch (e) {
+            that.showEstimate = false
+            that.updateUI()
+        }
+        }
+    }
+    xhr.open("GET", url, true);
+    xhr.send();
+}
+
+ShowFundingEstimate.prototype.calculateMonthsDifference = function(futureDate) {
+    var date = new Date();
+    var firstOfCurrentMonth = new Date(date.getFullYear(), date.getMonth(), 1);
+    var difference = (futureDate.getTime() - firstOfCurrentMonth.getTime()) / 1000;
+    difference = difference / (60 * 60 * 24 * 7 * 4);
+    return Math.round(difference) + 1;
+  }
+  
+ShowFundingEstimate.prototype.checkFieldValues = function () {
+    if (this.selectedStandardId.length > 0 && this.numberOfApprentices > 0 && this.monthsRemaining > 0) {
+      this.showEstimate = true
+      this.getEstimate()
+    } else {
+      this.showEstimate = false
+      this.updateUI()
+    }
+}
+  
+ShowFundingEstimate.prototype.updateUI = function () {
+
+    var infoPanel = document.getElementById('details-about-funding');
+    var estimatePanel = document.getElementById('details-about-funding-calculated');
+
+    if (this.showEstimate) {
+
+      //document.getElementById('field-estimate').innerText = this.estimate.toLocaleString()
+      infoPanel.style.display = 'none'
+      estimatePanel.style.display = 'block'
+
+    } else {
+      infoPanel.style.display = 'block'
+      estimatePanel.style.display = 'none'
+    }
+}
+
+var showFundingEstimate = new ShowFundingEstimate().init()
