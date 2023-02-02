7df7c8680bc7f8f156506e898fa2930c110f6bad
James King <hello@jamesmichaelking.com>
2023-02-02 10:24:12 +0000

    JS wip

--- src/SFA.DAS.Forecasting.Web/wwwroot/js/apprenticeship.js
+++ src/SFA.DAS.Forecasting.Web/wwwroot/js/apprenticeship.js
@@ -2,12 +2,16 @@ function ShowFundingEstimate() {
     this.apprenticeshipRoleSelectId = 'choose-apprenticeship'
     this.apprenticeshipRole = document.getElementById(this.apprenticeshipRoleSelectId)
     this.apprenticeshipNumber = document.getElementById('no-of-app')
+    this.numberOfMonthsField = document.getElementById('apprenticeship-length')
     this.apprenticeshipDateMonth = document.getElementById('startDateMonth')
     this.apprenticeshipDateYear = document.getElementById('startDateYear')
+    this.editMode = false
     this.selectedStandardId = 0
     this.numberOfApprentices = 0
-    this.startMonth = 0
-    this.startYear = 0
+    this.numberOfMonths = 0
+    this.startMonth = null
+    this.startYear = null
+    this.startDate = null
     this.monthsRemaining = 0
     this.estimate = 0
     this.showEstimate = false
@@ -27,6 +31,9 @@ ShowFundingEstimate.prototype.pageLoad = function () {
     if (this.apprenticeshipNumber.value !== "") {
       this.numberOfApprentices = this.apprenticeshipNumber.value
     }
+    if (this.numberOfMonthsField.value !== "") {
+      this.numberOfMonths = this.numberOfMonthsField.value
+    }
     if (this.apprenticeshipDateMonth.value !== "") {
       this.startMonth = this.apprenticeshipDateMonth.value
       this.dateChange()
@@ -44,21 +51,25 @@ ShowFundingEstimate.prototype.setupEvents = function () {
         that.numberOfApprentices = this.value | 0
         that.checkFieldValues()
     }
+    this.numberOfMonthsField.onkeyup = function () {
+      that.numberOfMonths = this.value | 0
+      that.checkFieldValues()
+  }
     this.apprenticeshipDateMonth.onkeyup = function () {
-        that.startMonth = this.value | 0
+        that.startMonth = this.value
         that.dateChange()
         that.checkFieldValues()
     }
     this.apprenticeshipDateYear.onkeyup = function () {
-        that.startYear = this.value | 0
+        that.startYear = this.value
         that.dateChange()
         that.checkFieldValues()
     }
 }
 
 ShowFundingEstimate.prototype.dateChange = function () {
-    var futureDate = new Date(this.startYear + ", " + this.startMonth + ", 1")
-    this.monthsRemaining = this.calculateMonthsDifference(futureDate);
+    this.startDate = new Date(this.startYear, this.startMonth - 1, 1)
+    this.monthsRemaining = this.calculateMonthsDifference(this.startDate);
 }
 
 
@@ -73,6 +84,7 @@ ShowFundingEstimate.prototype.autoComplete = function() {
         placeholder: '',
         showAllValues: true,
         onConfirm: function (opt) {
+            var roleChange = false
             var txtInput = document.querySelector('#' + that.apprenticeshipRoleSelectId);
             var searchString = opt || txtInput.value;
             var requestedOption = [].filter.call(this.selectElement.options,
@@ -83,7 +95,10 @@ ShowFundingEstimate.prototype.autoComplete = function() {
             if (requestedOption) {
                 requestedOption.selected = true;
                 if (requestedOption.value) {
-                that.selectedStandardId = requestedOption.value
+                  if (that.selectedStandardId != requestedOption.value) {
+                    roleChange = true
+                  }
+                  that.selectedStandardId = requestedOption.value
                 } else {
                 that.selectedStandardId = 0
                 }
@@ -91,37 +106,76 @@ ShowFundingEstimate.prototype.autoComplete = function() {
                 this.selectElement.selectedIndex = 0;
                 that.selectedStandardId = 0
             }
+
             that.checkFieldValues()
+            if (roleChange) {
+              that.roleHasChanged()
+            }
+            console.log('selectedStandardId = ' + that.selectedStandardId)
         }
     });
 }
 
-ShowFundingEstimate.prototype.getEstimate = function () {
+ShowFundingEstimate.prototype.getCourseApiUrl = function () {
+  var pathArray = window.location.pathname.split("/");
+  var indexMax = pathArray.length - (this.editMode ? 2 : 1); 
+  var newPathname = "";
+  for (i = 1; i < indexMax; i++) {
+      newPathname += "/";
+      newPathname += pathArray[i];
+  }
+  return window.location.origin + newPathname + "/course";
+}
+
+ShowFundingEstimate.prototype.roleHasChanged = function () {
     var that = this
-    var params = 'SelectedStandardId=' + this.selectedStandardId + '&NumberOfApprentices=' + this.numberOfApprentices + '&StartDate=' + this.startYear + '-' + this.startMonth + '-01'
-    var url = window.location.href.split('?')[0] + '/funding-estimate?' + params
+    var paramObj = {
+      CourseId: this.selectedStandardId
+    }
+    var params = new URLSearchParams(paramObj).toString();
+    var apiurl = this.getCourseApiUrl() + "?" + params
+     
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
-        try {
-            var response = JSON.parse(xhr.responseText)
-            var estimate = parseInt(response.amount)
-            if (isNaN(estimate)) {
-            that.estimate = 0
-            that.showEstimate = false
-            } else {
-            that.estimate = estimate
-            that.hasEnoughFunding = response.hasEnoughFunding
-            that.showEstimate = true
-            }
-            that.updateUI()
-        } catch (e) {
-            that.showEstimate = false
-            that.updateUI()
-        }
+          try {
+              var response = JSON.parse(xhr.responseText)
+              console.log(response.numberOfMonths)
+              that.numberOfMonthsField.value = response.numberOfMonths
+              that.numberOfMonths = response.numberOfMonths
+          } catch (e) {
+              console.log(e)
+          }
         }
     }
-    xhr.open("GET", url, true);
+    xhr.open("GET", apiurl, true);
+    xhr.send(JSON.stringify(paramObj));
+}
+
+ShowFundingEstimate.prototype.getEstimate = function () {
+    var that = this
+    var paramObj = {
+      CourseId: this.selectedStandardId,
+      NumberOfApprentices: this.numberOfApprentices,
+      NumberOfMonths: this.numberOfMonths,
+      StartDate: this.startDate
+    }
+    var params = new URLSearchParams(paramObj).toString();
+    var apiurl = this.getCourseApiUrl() + "?" + params
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          try {
+              var response = JSON.parse(xhr.responseText)
+              that.showEstimate = true
+              that.updateUI()
+          } catch (e) {
+              that.showEstimate = false
+              that.updateUI()
+          }
+        }
+    }
+    xhr.open("GET", apiurl, true);
     xhr.send();
 }
 
@@ -134,7 +188,7 @@ ShowFundingEstimate.prototype.calculateMonthsDifference = function(futureDate) {
   }
   
 ShowFundingEstimate.prototype.checkFieldValues = function () {
-    if (this.selectedStandardId.length > 0 && this.numberOfApprentices > 0 && this.monthsRemaining > 0) {
+    if (this.selectedStandardId.length > 0 && this.numberOfMonths > 0 && this.numberOfApprentices > 0 && this.monthsRemaining > 0) {
       this.showEstimate = true
       this.getEstimate()
     } else {
@@ -144,20 +198,12 @@ ShowFundingEstimate.prototype.checkFieldValues = function () {
 }
   
 ShowFundingEstimate.prototype.updateUI = function () {
-
-    var infoPanel = document.getElementById('details-about-funding');
-    var estimatePanel = document.getElementById('details-about-funding-calculated');
-
     if (this.showEstimate) {
-
-      //document.getElementById('field-estimate').innerText = this.estimate.toLocaleString()
-      infoPanel.style.display = 'none'
-      estimatePanel.style.display = 'block'
-
+      console.log('Show estimate')
     } else {
-      infoPanel.style.display = 'block'
-      estimatePanel.style.display = 'none'
+      console.log('Hide estimate')
     }
 }
 
-var showFundingEstimate = new ShowFundingEstimate().init()
+var showFundingEstimate = new ShowFundingEstimate()
+showFundingEstimate.init()
