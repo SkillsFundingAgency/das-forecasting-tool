d5e3b420328d6c8d157f202ac4ca442727a3d00c
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2023-04-05 08:58:36 +0100

    FAI-818 - Add stub authentication for gov login

--- src/SFA.DAS.Forecasting.Web/Controllers/ServiceController.cs
+++ src/SFA.DAS.Forecasting.Web/Controllers/ServiceController.cs
@@ -1,15 +1,28 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using SFA.DAS.Forecasting.Web.Configuration;
+using SFA.DAS.GovUK.Auth.Models;
+using SFA.DAS.GovUK.Auth.Services;
 
 namespace SFA.DAS.Forecasting.Web.Controllers
 {
     [Route("accounts")]
     public class ServiceController : Controller
     {
+        private readonly IConfiguration _config;
+        private readonly IStubAuthenticationService _stubAuthenticationService;
+
+        public ServiceController(IConfiguration config, IStubAuthenticationService stubAuthenticationService)
+        {
+            _config = config;
+            _stubAuthenticationService = stubAuthenticationService;
+        }
 
         [Route("signout", Name = RouteNames.SignOut)]
         public async Task<IActionResult> SignOutEmployer()
@@ -23,5 +36,35 @@ namespace SFA.DAS.Forecasting.Web.Controllers
                 authenticationProperties, CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
         }
 
+#if DEBUG
+        [HttpGet]
+        [Route("SignIn-Stub")]
+        [AllowAnonymous]
+        public IActionResult SigninStub()
+        {
+            return View("SigninStub", new List<string>{_config["StubId"],_config["StubEmail"]});
+        }
+        [HttpPost]
+        [Route("SignIn-Stub")]
+        [AllowAnonymous]
+        public IActionResult SigninStubPost()
+        {
+            _stubAuthenticationService?.AddStubEmployerAuth(Response.Cookies, new StubAuthUserDetails
+            {
+                Email = _config["StubEmail"],
+                Id = _config["StubId"]
+            }, true);
+
+            return RedirectToRoute("Signed-in-stub");
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("signed-in-stub", Name = "Signed-in-stub")]
+        public IActionResult SignedInStub()
+        {
+            return View();
+        }
+#endif
     }
 }
