88347ffe923321fee6091fe207c0c6c3336aad10
Deven Shah <devenshah1974@gmail.com>
2023-11-21 16:29:27 +0000

    CSP-1114 Deny access to users with role None (#405)

--- src/SFA.DAS.Forecasting.Web/AppStart/ConfigureEmployerAuthenticationExtension.cs
+++ src/SFA.DAS.Forecasting.Web/AppStart/ConfigureEmployerAuthenticationExtension.cs
@@ -11,7 +11,6 @@ using SFA.DAS.Forecasting.Application.EmployerUsers;
 using SFA.DAS.Forecasting.Core.Configuration;
 using SFA.DAS.Forecasting.Web.Authentication;
 using SFA.DAS.Forecasting.Web.Filters;
-using SFA.DAS.GovUK.Auth.AppStart;
 using SFA.DAS.GovUK.Auth.Authentication;
 using SFA.DAS.GovUK.Auth.Services;
 
@@ -26,11 +25,11 @@ public static class ConfigureEmployerAuthenticationExtension
         services.AddTransient<IEmployerAccountAuthorisationHandler, EmployerAccountAuthorizationHandler>();
         services.AddSingleton<IAuthorizationHandler, EmployerAccountAuthorizationHandler>();
         services.AddTransient<IEmployerAccountService, EmployerAccountService>();
-        
+
         //TODO to be removed after gov login enabled
         services.AddTransient<IAuthorizationHandler, AccountActiveAuthorizationHandler>();
         services.AddTransient<IStubAuthenticationService, StubAuthenticationService>();
-        
+
         services.AddAuthorization(options =>
         {
             options.AddPolicy(
@@ -44,63 +43,63 @@ public static class ConfigureEmployerAuthenticationExtension
                 });
         });
     }
-    
+
     public static void AddAndConfigureEmployerAuthentication(
             this IServiceCollection services,
             IdentityServerConfiguration configuration)
-        {
-            services
-                .AddAuthentication(sharedOptions =>
-                {
-                    sharedOptions.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    sharedOptions.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-                    sharedOptions.DefaultSignOutScheme = OpenIdConnectDefaults.AuthenticationScheme;
-
-                }).AddOpenIdConnect(options =>
-                {
-                    options.ClientId = configuration.ClientId;
-                    options.ClientSecret = configuration.ClientSecret;
-                    options.Authority = configuration.BaseAddress;
-                    options.MetadataAddress = $"{configuration.BaseAddress}/.well-known/openid-configuration";
-                    options.ResponseType = "code";
-                    options.UsePkce = false;
-                    
-                    options.Scope.Add("openid");
-                    options.Scope.Add("profile");
-                    
-                    options.ClaimActions.MapUniqueJsonKey("sub", "id");
-                    options.Events.OnRemoteFailure = c =>
-                    {
-                        if (c.Failure.Message.Contains("Correlation failed"))
-                        {
-                            c.Response.Redirect("/");
-                            c.HandleResponse();
-                        }
-    
-                        return Task.CompletedTask;
-                    };
-                });
-            services
-                .AddOptions<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme)
-                .Configure<ICustomClaims>((options, customClaims) =>
-                {
-                    options.Events.OnTokenValidated = async (ctx) =>
-                    {
-                        var claims = await customClaims.GetClaims(ctx);
-                        ctx.Principal.Identities.First().AddClaims(claims);
-                    };
-                });
-            
-            services.AddAuthentication().AddCookie(options =>
+    {
+        services
+            .AddAuthentication(sharedOptions =>
             {
-                options.AccessDeniedPath = new PathString("/error/403");
-                options.ExpireTimeSpan = TimeSpan.FromHours(1);
-                options.Cookie.Name = $"SFA.DAS.Forecasting.Web.Auth";
-                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-                options.SlidingExpiration = true;
-                options.Cookie.SameSite = SameSiteMode.None;
-                options.CookieManager = new ChunkingCookieManager { ChunkSize = 3000 };
+                sharedOptions.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                sharedOptions.DefaultSignOutScheme = OpenIdConnectDefaults.AuthenticationScheme;
+
+            }).AddOpenIdConnect(options =>
+            {
+                options.ClientId = configuration.ClientId;
+                options.ClientSecret = configuration.ClientSecret;
+                options.Authority = configuration.BaseAddress;
+                options.MetadataAddress = $"{configuration.BaseAddress}/.well-known/openid-configuration";
+                options.ResponseType = "code";
+                options.UsePkce = false;
+
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+
+                options.ClaimActions.MapUniqueJsonKey("sub", "id");
+                options.Events.OnRemoteFailure = c =>
+                {
+                    if (c.Failure.Message.Contains("Correlation failed"))
+                    {
+                        c.Response.Redirect("/");
+                        c.HandleResponse();
+                    }
+
+                    return Task.CompletedTask;
+                };
             });
-        }
+        services
+            .AddOptions<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme)
+            .Configure<ICustomClaims>((options, customClaims) =>
+            {
+                options.Events.OnTokenValidated = async (ctx) =>
+                {
+                    var claims = await customClaims.GetClaims(ctx);
+                    ctx.Principal.Identities.First().AddClaims(claims);
+                };
+            });
+
+        services.AddAuthentication().AddCookie(options =>
+        {
+            options.AccessDeniedPath = new PathString("/accessdenied");
+            options.ExpireTimeSpan = TimeSpan.FromHours(1);
+            options.Cookie.Name = $"SFA.DAS.Forecasting.Web.Auth";
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            options.SlidingExpiration = true;
+            options.Cookie.SameSite = SameSiteMode.None;
+            options.CookieManager = new ChunkingCookieManager { ChunkSize = 3000 };
+        });
+    }
 }
