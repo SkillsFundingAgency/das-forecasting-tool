8c953261baf375d6c56ee700ba3830a3ffbaf873
Jon Harris <jon.harris@fuitsu.com>
2025-03-12 07:55:58 +0000

    Tidy

--- src/SFA.DAS.Forecasting.Web/Startup.cs
+++ src/SFA.DAS.Forecasting.Web/Startup.cs
@@ -149,23 +149,6 @@ namespace SFA.DAS.Forecasting.Web
                 
                 await next();
                 
-                // if (context.Response.Headers.ContainsKey("X-Frame-Options"))
-                // {
-                //     context.Response.Headers.Remove("X-Frame-Options");
-                // }
-                //
-                // context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
-                //
-                // await next();
-                //
-                // if (context.Response.StatusCode == 404 && !context.Response.HasStarted)
-                // {
-                //     //Re-execute the request so the user gets the error page
-                //     var originalPath = context.Request.Path.Value;
-                //     context.Items["originalPath"] = originalPath;
-                //     context.Request.Path = "/error/404";
-                //     await next();
-                // }
             });
 
             app.UseRouting();
